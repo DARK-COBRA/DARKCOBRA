b9a40d3a60c01d58bd41bb058d072c482ef12a52
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-06-28 20:24:15 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -92,14 +92,15 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
     number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
+    multi = Config.EMOJI_TO_DISPLAY_IN_HELP
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {} {}".format(Config.EMOJI_TO_DISPLAY_IN_HELP, x, Config.EMOJI_TO_DISPLAY_IN_HELP),
-        data="us_plugin_{}".format(x))
+        "{} {} {}".format(random.choice(list(multi)), x, random.choice(list(multi))),
+      data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     if number_of_cols == 1:
         pairs = list(zip(modules[::number_of_cols]))
