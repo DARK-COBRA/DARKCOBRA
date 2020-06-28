8c4dbfec6af3c0196a2582bdbd0b13e2188b7a19
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-06-28 22:38:19 +0530

    ＤａＲｋ　ｐＲｉＮｃＥ
    @uSe_DaRk_PrInCe

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,10 +1,4 @@
 
-# thanks to @WhySooSerious 
-# thanks to @Sur_vivor  
-# final editing by leobrownlee
-
-
-
 """Check if userbot alive or not . 
 
 """
@@ -17,26 +11,26 @@ from userbot.utils import admin_cmd
 from telethon import version
 from platform import python_version, uname
 
-ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
-if ALIVE_PIC is None:
-  MOD_IMG = "https://telegra.ph/file/189e8ac9f26e1fcff7e36.mp4"
+ALIVE_IMG = Config.ALIVE_PHOTTO
+if ALIVE_IMG is None:
+  ALIVE_IMG = "https://telegra.ph/file/3952f58c07382778cab87.jpg"
 else:
-  MOD_IMG = ALIVE_PIC
+  ALIVE_PHOTTO = ALIVE_IMG
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
-
-
-mod_caption = "**MY BOT IS RUNNING SUCCESFULLY\n\n\n**"
-mod_caption += "`🔰SYSTEM STATUS\n\n`"
-mod_caption += f"`🔰Telethon version: {version.__version__}\n\n`"
-mod_caption += "`🔰Database Status: Databases functioning normally!\n\n`"
-mod_caption += f"`🔰Python: {python_version()}\n\n`"
-mod_caption += "`🔰Always with you, my master!\n\n`"
-mod_caption += f"`🔰Owner Name`:   {DEFAULTUSER}\n\n\n"
-mod_caption += "`🔰Bot was modified by:` leobrownlee and Sur_vivor\n\n"
-mod_caption += "[DEPLOY MODIFIED FRIDAY](https://github.com/leobrownlee/FRIDAY)"
+ALIVE_MESSAGE = Config.ALIVE_MSG
+else:
+  ALIVE_MESSAGE = alive_caption
+alive_caption = "**ＤａＲｋ　ｐＲｉＮｃＥ IS Still Running\n\n**"
+alive_caption += "`BOT STATUS\n\n`"
+alive_caption += f"`Telethon version: {version.__version__}\n`"
+alive_caption += "`Database Status: ERROR! DATABASE NOT FOUND!\n`"
+alive_caption += f"`Python: PYTHON-3.6.4 \n`"
+alive_caption += "`I'll Be With You Till My Dynos Ends!!\n`"
+alive_caption += f"`Bot Created And Maintained By`:   {DEFAULTUSER}\n\n"
+alive_caption += "[DEPLOY This Lit Userbot Now](https://github.com/No-OnE-Kn0wS-Me/dArK_pRiNcE)"
 
 
 
@@ -45,4 +39,4 @@ mod_caption += "[DEPLOY MODIFIED FRIDAY](https://github.com/leobrownlee/FRIDAY)"
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.delete() 
-    await borg.send_file(alive.chat_id, MOD_IMG,caption=mod_caption)
+    await borg.send_file(alive.chat_id, ALIVE_IMG,caption=ALIVE_MESSAGE)
