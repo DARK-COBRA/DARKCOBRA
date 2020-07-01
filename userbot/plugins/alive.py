15f44a183e3d9adec10ef9f55e4737be8f75e3cc
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-01 23:22:09 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -42,23 +42,13 @@ if ALIVE_MESSAGE is None:
   ALIVE_MESSAGE += f"`Telethon version: Telethon Not Found\n`"
   ALIVE_MESSAGE += f"`Python: PYTHON-3.6.4 \n\n`"
   ALIVE_MESSAGE += "`I'll Be With You Till My Dyno Ends!!\n\n`"
-  ALIVE_MESSAGE += f"`Support Channel` : @uSe_DaRk_PrInCe  {BUTTONS} \n\n"
+  ALIVE_MESSAGE += f"`Support Channel` : @uSe_DaRk_PrInCe \n\n"
   ALIVE_MESSAGE += f"`Bot Created And Maintained By`:   {DEFAULTUSER}\n\n "
                 
-
-buttons=[
-                    [custom.Button.url("👤Contact Creator👤", "https://telegram.dog/r4v4n4"), custom.Button.url(
-                        "📼Ravana Audio Memes📼", "https://t.me/tgaudiomemes")],
-                    [custom.Button.url("👨‍💻Source Code👨‍💻", "https://github.com/ravana69/Pornhub"), custom.Button.url(
-                        "❕❗Deploy Me❗❕", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F&template=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F")],
-                    [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature"), custom.Button.url(
-                        "✳️Fork Boost✳️", "tg://some_unsupported_feature"), custom.Button.url(
-                        "📤Cloud Torrent📥", "https://github.com/ravana69/oneclickrun")]
-                ]
             
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.delete() 
-    await borg.send_file(alive.chat_id, ALIVE_IMG,caption=ALIVE_MESSAGE)
+    await borg.send_file(alive.chat_id, ALIVE_IMG,caption=ALIVE_MESSAGE,buttons=BUTTONS)
