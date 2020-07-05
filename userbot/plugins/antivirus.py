41d22e2e690c9f25e4a140fa6c91a210f38040bf
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-05 08:55:12 +0530

    Update antivirus.py

--- userbot/plugins/antivirus.py
+++ userbot/plugins/antivirus.py
@@ -28,7 +28,7 @@ async def _(event):
               await borg.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+              await event.reply("```Please unblock @DrWebBot and try again```")
               return
           if response.text.startswith("Forward"):
              await event.edit("```can you kindly disable your forward privacy settings for good?```")
