9e080e2c844bb5430b6341cd03c2a8a21319894c
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-21 17:26:06 +0530

    Create __init__.py

---
+++ userbot/plugins/__init__.py
@@ -0,0 +1,12 @@
+from userbot import catdef
+
+deEmojify = catdef.deEmojify
+trumptweet = catdef.trumptweet 
+changemymind = catdef.changemymind
+kannagen = catdef.kannagen
+moditweet = catdef.moditweet
+tweets = catdef.tweets
+waifutxt = catdef.waifutxt
+catmusic = catdef.catmusic 
+catmusicvideo = catdef.catmusicvideo
+admin_groups = catdef.admin_groups
