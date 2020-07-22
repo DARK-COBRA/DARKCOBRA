fff04c9721752a460d4974e1c1ed265b154d2291
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-22 11:12:43 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -21,10 +21,33 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 text="{}\nCurrently Loaded Plugins: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
-                link_preview=False
+                link_preview=True
+           ) 
+        elif query.startswith("repo"):
+            result = builder.article(
+                "@No_OnE_Kn0wS_Me : Source ",
+                text=f"ＤａＲｋ ｐＲｉＮｃＥ Repo",
+                buttons=[
+                    [custom.Button.url("👤Click on the button to deploy dark Prince repo👤", "https://github.com/No-OnE-Kn0wS-Me/dArK_pRiNcE")],
+                ],
+                link_preview=True
             )
+        elif query.startswith("alive"):
+            result = builder.article(
+                "@uSe_DaRk_PrInCe ",
+                text=f"ＤａＲｋ ｐＲｉＮｃＥ Is Alive",
+                buttons=[
+                    [custom.Button.url("👤Contact Creator👤", "https://telegram.dog/No_OnE_Kn0wS_Me"), custom.Button.url(
+                        "Support channel✔", "https://t.me/@uSe_DaRk_PrInCe")],
+                    [custom.Button.url("👨‍💻Source Code👨‍💻", "https://github.com/No-OnE-Kn0wS-Me/dArK_pRiNcE"), custom.Button.url(
+                        "❕❗Deploy Me❗❕", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F&template=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F")],
+                    [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature"), custom.Button.url(
+                        "✳️Fork Boost✳️", "tg://some_unsupported_feature")]
+                ],
+                link_preview=True
+            )     
         await event.answer([result] if result else None)
-    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+@tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"helpme_next\((.+?)\)")
     ))
     async def on_plug_in_callback_query_handler(event):
