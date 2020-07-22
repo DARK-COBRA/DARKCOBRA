b2e8444f5c12f39fc290e65838717687adc3bb00
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-22 18:30:20 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -1,51 +1,177 @@
-
+import requests
+from bs4 import BeautifulSoup
 from telethon import events
 import subprocess
 from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
 import io
 import asyncio
-import time
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd , sudo_cmd
 import glob
-import os
-try:
- import instantmusic , subprocess
-except:
- os.system("pip install instantmusic")
- 
+import os  
+from userbot import CMD_HELP
+from userbot.plugins import catmusic , catmusicvideo
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from telethon.tl.types import DocumentAttributeVideo
 
-
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
-    
-    os.system("instantmusic -q -s "+name)
-    
-
-@borg.on(admin_cmd(pattern="song ?(.*)"))
+@borg.on(admin_cmd(pattern="song(?: |$)(.*)"))
 async def _(event):
-    if event.fwd_from:
-        return
-    DELAY_BETWEEN_EDITS = 0.3
-    PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("`i am going to find your song..🎶`"),
-    bruh(str(cmd))
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        await event.edit("wi8..! I am finding your song....")
+    elif reply.message:
+        query = reply.message
+        await event.edit("wi8..! I am finding your song....")
+    else:
+    	await event.edit("`What I am Supposed to find `")
+    	return
+    
+    await catmusic(str(query),"128k")
     l = glob.glob("*.mp3")
-    loa = l[0]
-    await event.edit("`yeeeaah..! i found something..🎉`")
+    if l:
+        await event.edit("yeah..! i found something wi8..🥰")
+    else:
+        await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    loa = l[0]    
     await borg.send_file(
                 event.chat_id,
                 loa,
                 force_document=True,
                 allow_cache=False,
-                caption="`📁File successfully Uploaded by` : [ＤａＲｋ ｐＲｉＮｃＥ](https://t.me/uSe_DaRk_PrInCe) 🔱",
+                caption=query,
                 reply_to=reply_to_id
             )
     await event.delete()
     os.system("rm -rf *.mp3")
     subprocess.check_output("rm -rf *.mp3",shell=True)
+    
+@borg.on(admin_cmd(pattern="song320(?: |$)(.*)"))
+async def _(event):
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        await event.edit("wi8..! I am finding your song....")
+    elif reply.message:
+        query = reply.message
+        await event.edit("wi8..! I am finding your song....")
+    else:
+    	await event.edit("`What I am Supposed to find `")
+    	return
+    
+    await catmusic(str(query),"320k")
+    l = glob.glob("*.mp3")
+    if l:
+        await event.edit("yeah..! i found something wi8..🥰")
+    else:
+        await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    loa = l[0]    
+    await borg.send_file(
+                event.chat_id,
+                loa,
+                force_document=True,
+                allow_cache=False,
+                caption=query,
+                reply_to=reply_to_id
+            )
+    await event.delete()
+    os.system("rm -rf *.mp3")
+    subprocess.check_output("rm -rf *.mp3",shell=True)    
+    
+@borg.on(admin_cmd(pattern="videosong(?: |$)(.*)"))
+async def _(event):
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        await event.edit("wi8..! I am finding your videosong....")
+    elif reply.message:
+        query = reply.message
+        await event.edit("wi8..! I am finding your videosong....")
+    else:
+        await event.edit("What I am Supposed to find")
+        return
+    await catmusicvideo(query)
+    l = glob.glob(("*.mp4")) + glob.glob(("*.mkv")) + glob.glob(("*.webm")) 
+    if l:
+        await event.edit("yeah..! i found something wi8..🥰")
+    else:
+        await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    loa = l[0]  
+    metadata = extractMetadata(createParser(loa))
+    duration = 0
+    width = 0
+    height = 0
+    if metadata.has("duration"):
+        duration = metadata.get("duration").seconds
+    if metadata.has("width"):
+        width = metadata.get("width")
+    if metadata.has("height"):
+        height = metadata.get("height")
+    await borg.send_file(
+                event.chat_id,
+                loa,
+                force_document=True,
+                allow_cache=False,
+                caption=query,
+                supports_streaming=True,
+                reply_to=reply_to_id,
+                attributes=[DocumentAttributeVideo(
+                                duration=duration,
+                                w=width,
+                                h=height,
+                                round_message=False,
+                                supports_streaming=True,
+                            )],
+            )
+    await event.delete()
+    os.system("rm -rf *.mkv")
+    os.system("rm -rf *.mp4")
+    os.system("rm -rf *.webm")    
+    
+@borg.on(sudo_cmd(pattern="song(?: |$)(.*)", allow_sudo = True))
+async def _(event):
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        san = await event.reply("wi8..! I am finding your song....`")
+    elif reply.message:
+        query = reply.message
+        san = await event.reply("wi8..! I am finding your song....`")
+    else:
+    	san = await event.reply("`What I am Supposed to find `")
+    	return
+    await catmusic(str(query),"320k")
+    l = glob.glob("*.mp3")
+    if l:
+        await event.edit("yeah..! i found something wi8..🥰")
+    else:
+        await event.edit(f"Sorry..! i can't find anything with `{query}`")
+    loa = l[0]
+    await borg.send_file(
+                event.chat_id,
+                loa,
+                force_document=True,
+                allow_cache=False,
+                caption=query,
+                reply_to=reply_to_id
+            )
+    await san.delete()
+    os.system("rm -rf *.mp3")
+    subprocess.check_output("rm -rf *.mp3",shell=True)
+    
+CMD_HELP.update({"getmusic":
+    "`.song` query or `.song` reply to song name :\
+    \nUSAGE:finds the song you entered in query and sends it"
+})    
