b5c22a95562b71e32f25baaedc231f40350a8062
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-22 09:30:57 +0530

    Update and rename alive.py to awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -1,5 +1,5 @@
 
-"""Check if userbot alive or not . 
+"""Check if userbot awake or not . 
 
 """
 import os
@@ -26,13 +26,13 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 
 ALIVE_MESSAGE = Config.ALIVE_MSG
 if ALIVE_MESSAGE is None:
-  ALIVE_MESSAGE = "**🔱ＤａＲｋ　ｐＲｉＮｃＥ IS still Alive🔱\n\n\n**"
+  ALIVE_MESSAGE = "**🔱ＤａＲｋ　ｐＲｉＮｃＥ IS Awake 🔱\n\n\n**"
   ALIVE_MESSAGE += "`My Bot Status\n\n\n`"
   ALIVE_MESSAGE += f"`Telethon: Telethon Not Found\n\n`"
   ALIVE_MESSAGE += f"`Python: PYTHON-3.6.4 \n\n`"
   ALIVE_MESSAGE += "`I'll Be With You Master Till My Dyno Ends!!\n\n`"
   ALIVE_MESSAGE += f"`Support Channel` : @uSe_DaRk_PrInCe \n\n"
-  ALIVE_MESSAGE += f"`Bot Created And Maintained By`:   {DEFAULTUSER}\n\n "
+  ALIVE_MESSAGE += f"`Bot Created And Maintained By`: {DEFAULTUSER}\n\n "
                 
             
 #@command(outgoing=True, pattern="^.alive$")
