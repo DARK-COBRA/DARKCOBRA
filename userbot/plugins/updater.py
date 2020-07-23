364c1ab3366cd9257b8258a53842fd1600e47d65
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-23 13:28:09 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -1,42 +1,68 @@
-"""Update UserBot code
-Syntax: .update"""
+"""Update UserBot code (for Xtra-Telegram)
+Syntax: .update
+\nAll Credits goes to © @Three_Cube_TeKnoways
+\nFor this awasome plugin.\nPorted from PpaperPlane Extended"""
 
+from os import remove,execl
+import sys
 import git
+import asyncio
+import random
+import re
+import time
+from collections import deque
+import requests
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon import events
+from userbot.utils import admin_cmd
 from contextlib import suppress
 import os
-import sys
-import asyncio
-from userbot.utils import admin_cmd
+
 
 # -- Constants -- #
 IS_SELECTED_DIFFERENT_BRANCH = (
-    "Looks like a custom branch {branch_name} "
-    "Is being used:\n"
-    "In this case, Updater is unable to identify the branch to be updated."
-    "Please check out to an official branch, and re-start the updater."
+    "looks like a custom branch {branch_name} "
+    "is being used:\n"
+    "in this case, Updater is unable to identify the branch to be updated."
+    "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/No-OnE-Kn0wS-Me/dArK_pRiNcE/"
-BOT_IS_UP_TO_DATE = "**Boss! My System is already Upgraded!**."
+
+OFFICIAL_UPSTREAM_REPO = "https://github.com/No-OnE-Kn0wS-Me/dArK_pRiNcE.git"
+
+BOT_IS_UP_TO_DATE = "`The userbot is up-to-date.\nThank you for Using this Service.`"
 NEW_BOT_UP_DATE_FOUND = (
-    "Boss I found update for {branch_name}\n"
-    "{changelog}\n"
-    "**Ok Boss ! I Am Trying To Pull Update**"
+    "new update found for {branch_name}\n"
+    "changelog: \n\n{changelog}\n"
+    "updating ..."
 )
 NEW_UP_DATE_FOUND = (
-    "**Boss I Found new update found for {branch_name}**\n"
-    "`i am updating ...`"
+    "New update found for {branch_name}\n"
+    "`updating ...`"
 )
 REPO_REMOTE_NAME = "temponame"
 IFFUCI_ACTIVE_BRANCH_NAME = "master"
 DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
-NO_HEROKU_APP_CFGD = "Boss ! no heroku application found, but a key given? 😕 "
+NO_HEROKU_APP_CFGD = "no heroku application found, but a key given? 😕 "
 HEROKU_GIT_REF_SPEC = "HEAD:refs/heads/master"
-RESTARTING_APP = "Boss ! I am Restarting "
+RESTARTING_APP = "re-starting heroku application"
 # -- Constants End -- #
 
+async def generate_change_log(git_repo, diff_marker):
+    out_put_str = ""
+    d_form = "%d/%m/%y"
+    for repo_change in git_repo.iter_commits(diff_marker):
+        out_put_str += f"•[{repo_change.committed_datetime.strftime(d_form)}]: {repo_change.summary} <{repo_change.author}>\n"
+    return out_put_str
 
-#@command(pattern="^.update", outgoing=True)
-@borg.on(admin_cmd(pattern=r"update"))
+async def deploy_start(tgbot, message, refspec, remote):
+    await message.edit(RESTARTING_APP)
+    await message.edit("Updating and Deploying New Branch. Please wait for 5 minutes then use `.alive` to check if i'm working or not.")
+    await  remote.push(refspec="HEAD:refs/heads/master", force=True)
+    await tgbot.disconnect()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    
+@borg.on(admin_cmd(pattern="update ?(.*)", outgoing=True))
 async def updater(message):
     try:
         repo = git.Repo()
@@ -53,17 +79,14 @@ async def updater(message):
             branch_name=active_branch_name
         ))
         return False
-
     try:
         repo.create_remote(REPO_REMOTE_NAME, OFFICIAL_UPSTREAM_REPO)
     except Exception as e:
         print(e)
         pass
-
     temp_upstream_remote = repo.remote(REPO_REMOTE_NAME)
     temp_upstream_remote.fetch(active_branch_name)
-
-    changelog = generate_change_log(
+    changelog = await generate_change_log(
         repo,
         DIFF_MARKER.format(
             remote_name=REPO_REMOTE_NAME,
@@ -72,7 +95,7 @@ async def updater(message):
     )
 
     if not changelog:
-        await message.edit("**Boss i Found UPDATE Let me Upgrade Myself To Serve You Better!!...**")
+        await message.edit("`Updating...`")
         await asyncio.sleep(8)
  
     message_one = NEW_BOT_UP_DATE_FOUND.format(
@@ -86,7 +109,7 @@ async def updater(message):
     if len(message_one) > 4095:
         with open("change.log", "w+", encoding="utf8") as out_file:
             out_file.write(str(message_one))
-        await bot.send_message(
+        await tgbot.send_message(
             message.chat_id,
             document="change.log",
             caption=message_two
@@ -94,10 +117,8 @@ async def updater(message):
         os.remove("change.log")
     else:
         await message.edit(message_one)
-
     temp_upstream_remote.fetch(active_branch_name)
     repo.git.reset("--hard", "FETCH_HEAD")
-
     if Var.HEROKU_API_KEY is not None:
         import heroku3
         heroku = heroku3.from_key(Var.HEROKU_API_KEY)
@@ -109,7 +130,7 @@ async def updater(message):
                     if i.name == Var.HEROKU_APP_NAME:
                         heroku_app = i
                 if heroku_app is None:
-                    await message.edit("Boss ! Invalid APP Name. Boss Please set the name of your bot in heroku in the var HEROKU_APP_NAME.")
+                    await message.edit("Invalid APP Name. Please set the name of your bot in heroku in the var `HEROKU_APP_NAME.`")
                     return
                 heroku_git_url = heroku_app.git_url.replace(
                     "https://",
@@ -120,29 +141,11 @@ async def updater(message):
                     remote.set_url(heroku_git_url)
                 else:
                     remote = repo.create_remote("heroku", heroku_git_url)
-                asyncio.get_event_loop().create_task(deploy_start(bot, message, HEROKU_GIT_REF_SPEC, remote))
-
+                asyncio.get_event_loop().create_task(deploy_start(tgbot, message, HEROKU_GIT_REF_SPEC, remote))
             else:
-                await message.edit("Boss ! Please create the var HEROKU_APP_NAME as the key and the name of your bot in heroku as your value.")
+                await message.edit("Please create the var `HEROKU_APP_NAME` as the key and the name of your bot in heroku as your value.")
                 return
         else:
             await message.edit(NO_HEROKU_APP_CFGD)
     else:
-        await message.edit("Boss ! I Found No heroku api key found in HEROKU_API_KEY var")
-        
-
-def generate_change_log(git_repo, diff_marker):
-    out_put_str = ""
-    d_form = "%d/%m/%y"
-    for repo_change in git_repo.iter_commits(diff_marker):
-        out_put_str += f"❥︎[{repo_change.committed_datetime.strftime(d_form)}]➪︎ {repo_change.summary} ✪︎{repo_change.author}\n"
-    return out_put_str
-
-async def deploy_start(bot, message, refspec, remote):
-    await message.edit(RESTARTING_APP)
-    await message.edit("**Boss! I Am Upgraded Now I Am Trying A Restart do** `.alive` **to check if I am Alive Or Dead**\nIt will takes approximately 5 mins to update My Software!!\nBoss Thank You For Using Me You Are My Best Boss!!,")
-    await remote.push(refspec=refspec)
-    await bot.disconnect()
-    os.execl(sys.executable, sys.executable, *sys.argv)
-
-    
+        await message.edit("No heroku api key found in `HEROKU_API_KEY` var")
