511dd6ed535e80bcdc4e4b9606c5c5959f3a2077
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-23 13:16:37 +0530

    Update heroku.py

--- userbot/plugins/heroku.py
+++ userbot/plugins/heroku.py
@@ -174,14 +174,14 @@ async def _(dyno):
         await dyno.edit("Got the logs wait a sec")    
         await dyno.client.send_file(
             dyno.chat_id,
-            "logs.txt",
+            "@uSe_DaRk_PrInCe logs.txt",
             reply_to=dyno.id,
             caption="logs of 100+ lines",
         )
         
         await asyncio.sleep(5)
         await dyno.delete()
-        return os.remove('logs.txt')
+        return os.remove('@uSe_DaRk_PrInCe logs.txt')
     
 def prettyjson(obj, indent=2, maxlinelength=80):
     """Renders JSON content with indentation and line splits/concatenations to fit maxlinelength.
