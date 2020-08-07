600fef391add347dc34a35dd46c75321a82dad0a
Crazy Creator <59717478+crazycreatorr@users.noreply.github.com>
2020-08-07 17:14:34 +0530

    Update Randompics.py

--- userbot/plugins/Randompics.py
+++ userbot/plugins/Randompics.py
@@ -75,7 +75,7 @@ async def autopic(event):
     while True:
         piclink = random.randint(0, len(TELEGRAPH_MEDIA_LINKS) - 1)
         AUTOPP = TELEGRAPH_MEDIA_LINKS[piclink]
-        downloaded_file_name = "./ravana/original_pic.png"
+        downloaded_file_name = "./DOWNLOADS/original_pic.png"
         downloader = SmartDL(AUTOPP, downloaded_file_name, progress_bar=True)
         downloader.start(blocking=False)
         photo = "photo_pfp.png"
