fa2db8047abf905a975d3264ab4ee6332d77cb11
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-08-08 16:36:42 +0530

    Update bye.py

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -11,9 +11,9 @@ import time
 @borg.on(admin_cmd("bye", outgoing=True))
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("`I am leaving this chat.....!`")
+        await e.edit("`I am leaving this chat😒..koi ni hai yaha apna I hate you all🙄..huh!!`")
         time.sleep(3)
         if '-' in str(e.chat_id):
             await borg(LeaveChannelRequest(e.chat_id))
         else:
-            await e.edit('`Sir This is Not A Chat`')
+            await e.edit('`Mere pyaare boss ye chat nahi hai..thoda soch samajh kar command dijiye..🤥`')
