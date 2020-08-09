b463896998381ff2676b2c444ebdedb88de5758c
Ultimate-Don <69391398+Ultimate-Don@users.noreply.github.com>
2020-08-09 13:41:32 +0500

    Update covid_v2.py

--- userbot/plugins/covid_v2.py
+++ userbot/plugins/covid_v2.py
@@ -7,7 +7,7 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
 
 @borg.on(admin_cmd(pattern="covid ?(.*)"))
 async def _(event):
