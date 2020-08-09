262041aff94fc2786085ba328bdc75923b3f136b
Crazy Creator <59717478+crazycreatorr@users.noreply.github.com>
2020-08-09 09:34:37 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -74,7 +74,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = help_string
         reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
-            © ＤａＲｋ　ｐＲｉＮｃＥ".format(plugin_name)
+            ©DARKCOBRA".format(plugin_name)
         try:
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
         except:
