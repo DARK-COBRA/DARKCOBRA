43bd95eaa99a1eb8da5a095d42d86275d21cbc59
Crazy Creator <59717478+crazycreatorr@users.noreply.github.com>
2020-08-09 11:26:07 +0530

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -36,7 +36,7 @@ async def _(event):
     await borg.send_file(  # pylint:disable=E0602
         event.chat_id,
         required_file_name,
-        caption="Userbot: Powered by @pureindialover",
+        caption="Userbot: Powered by @Dark_cobra_support",
         # Courtesy: @ManueI15
         reply_to=reply_msg_id
     )
