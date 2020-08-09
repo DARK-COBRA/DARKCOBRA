0860267227206ffc0e7ea5a6878c40ebe6dc6fe4
J.A.R.V.I.S <68327188+jarvis210904@users.noreply.github.com>
2020-08-09 23:04:59 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -49,8 +49,20 @@ if Var.PRIVATE_GROUP_ID is not None:
                 await event.edit("Approved to pm [{}](tg://user?id={})".format(firstname, chat.id))
                 await asyncio.sleep(3)
                 await event.delete()
-
-
+        @bot.on(events.NewMessage(outgoing=True))	    @command(pattern="^.block ?(.*)")
+    async def you_dm_niqq(event):	
+        if event.fwd_from:	
+            return	
+        chat = await event.get_chat()	
+        if event.is_private:	
+            if not pmpermit_sql.is_approved(chat.id):	
+                if not chat.id in PM_WARNS:	
+                    pmpermit_sql.approve(chat.id, "outgoing")	
+                    bruh = "__Added user to approved pms cuz outgoing message >~<__"	
+                    rko = await borg.send_message(event.chat_id, bruh)	
+                    await asyncio.sleep(3)	
+                    await rko.delete()	
+    
     @command(pattern="^.block ?(.*)")
     async def approve_p_m(event):
         if event.fwd_from:
