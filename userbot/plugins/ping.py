5de243889ec68ca01bf25c75aae7f5609e54bfd0
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-08-10 07:35:25 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -10,4 +10,4 @@ async def _(event):
     await event.edit("Pong!๏๏")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit("★ Pong!..\n{}".format(ms))
+    await event.edit("★ Pong!༒...✦҈͜͡➳\n{}".format(ms))
