317d681aaab82ea6856cff2f36ede27e8afe7c96
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-08-16 11:50:21 +0530

    Create functions.py

---
+++ userbot/helpers/functions.py
@@ -0,0 +1,68 @@
+import requests , os, re
+from bs4 import BeautifulSoup
+from asyncio import sleep
+from random import choice
+from telethon import events
+from emoji import get_emoji_regexp
+from PIL import Image
+from validators.url import url
+from telethon.tl.types import Channel
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+
+#for songs.py
+
+async def darkmusic(dark , QUALITY):
+  search = dark
+  headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+  html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
+  soup = BeautifulSoup(html, 'html.parser')
+  for link in soup.find_all('a'):
+    if '/watch?v=' in link.get('href'):
+        # May change when Youtube Website may get updated in the future.
+        video_link = link.get('href') 
+        break
+  video_link =  'http://www.youtube.com/'+video_link
+  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' + QUALITY + ' ' + video_link)	
+  os.system(command)
+
+
+async def darkmusicvideo(dark):
+    search = dark
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+    html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
+    soup = BeautifulSoup(html, 'html.parser')
+    for link in soup.find_all('a'):
+        if '/watch?v=' in link.get('href'):
+            # May change when Youtube Website may get updated in the future.
+            video_link = link.get('href') 
+            break    
+    video_link =  'http://www.youtube.com/'+video_link
+    command = ('youtube-dl -f "[filesize<20M]" ' +video_link)  
+    os.system(command)
