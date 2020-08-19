78a4a25b23c9e79c0cbf6e7c92360fe57efd855f
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-08-19 20:31:34 +0530

    Update stickercreator.py

--- userbot/plugins/stickcreater.py
+++ userbot/plugins/stickcreater.py
@@ -54,7 +54,7 @@ async def sticklet(event):
     image.save(image_stream, "WebP")
     image_stream.seek(0)
     # finally, reply the sticker
-    await event.client.send_file(event.chat_id, image_stream, caption="Created Using Sensible Userbot Userbot", reply_to=event.message.reply_to_msg_id)
+    await event.client.send_file(event.chat_id, image_stream, caption="Created Using DARK COBRA THE USERBOT", reply_to=event.message.reply_to_msg_id)
     # cleanup
     try:
         os.remove(FONT_FILE)
@@ -104,7 +104,7 @@ async def sticklet(event):
     image.save(image_stream, "WebP")
     image_stream.seek(0)
     # finally, reply the sticker
-    await event.client.send_file(event.chat_id, image_stream, caption="Created Using Sensible Userbot", reply_to=event.message.reply_to_msg_id)
+    await event.client.send_file(event.chat_id, image_stream, caption="Created Using DARK COBRA THE USERBOT", reply_to=event.message.reply_to_msg_id)
     # cleanup
     try:
         os.remove(FONT_FILE)
