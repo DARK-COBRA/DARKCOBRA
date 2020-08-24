89de14b2f6b433e13bd3529b546975e4489d01db
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-08-24 14:17:08 +0530

    Update get_music.py

--- userbot/plugins/get_music.py
+++ userbot/plugins/get_music.py
@@ -8,7 +8,7 @@ import asyncio
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("smd (.*)"))
+@borg.on(admin_cmd("gaana (.*)"))
 async def _(event):
     if event.fwd_from:
         return
