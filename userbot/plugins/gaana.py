5449ec6acf3f2217405932646009b5459f658874
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-08-25 08:09:55 +0530

    Update gaana.py

--- userbot/plugins/gaana.py
+++ userbot/plugins/gaana.py
@@ -1,5 +1,5 @@
-# Originally from Bothub
-# Port to UserBot by @heyworld
+
+# Port to UserBot
 #ported and modified to DARK COBRA by @hellboi-atul
 #Copyright (C) 2020 azrim.
 
@@ -26,26 +26,6 @@ def bruh(name):
     
 
 
-@register(outgoing=True, pattern="^.spd(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    link = event.pattern_match.group(1)
-    chat = "@SpotifyMusicDownloaderBot"
-    await event.edit("```Getting Your Music```")
-    async with bot.conversation(chat) as conv:
-          await asyncio.sleep(2)
-          await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
-          try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              await bot.send_message(chat, link)
-              respond = await response
-          except YouBlockedUserError:
-              await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
-              return
-          await event.delete()
-          await bot.forward_messages(event.chat_id, respond.message)
-
 @register(outgoing=True, pattern="^.netease(?: |$)(.*)")
 async def WooMai(netase):
     if netase.fwd_from:
@@ -53,10 +33,10 @@ async def WooMai(netase):
     song = netase.pattern_match.group(1)
     chat = "@WooMaiBot"
     link = f"/netease {song}"
-    await netase.reply("```Getting Your Music```")
+    await netase.reply("```Let me search for the music you are looking for..```")
     async with bot.conversation(chat) as conv:
           await asyncio.sleep(2)
-          await netase.reply("`Downloading...Please wait`")
+          await netase.reply("`Downloading...it would take a few seconds..stay tuned`")
           try:
               msg = await conv.send_message(link)
               response = await conv.get_response()
@@ -66,7 +46,7 @@ async def WooMai(netase):
           except YouBlockedUserError:
               await netase.reply("```Please unblock @WooMaiBot and try again```")
               return
-          await netase.edit("`Sending Your Music...`")
+          await netase.reply("`Sending your music🤗...Thanks for using DARK COBRA song plugin..`")
           await asyncio.sleep(3)
           await bot.send_file(netase.chat_id, respond)
     await netase.client.delete_messages(conv.chat_id,
@@ -74,47 +54,12 @@ async def WooMai(netase):
     await netase.delete()
 
 
-@register(outgoing=True, pattern="^.dzd(?: |$)(.*)")
-async def DeezLoader(Deezlod):
-    if Deezlod.fwd_from:
-        return
-    d_link = Deezlod.pattern_match.group(1)
-    if ".com" not in d_link:
-        await Deezlod.edit("` I need a link to download something pro.`**(._.)**")
-    else:
-        await Deezlod.edit("**Initiating Download!**")
-    chat = "@DeezLoadBot"
-    async with bot.conversation(chat) as conv:
-          try:
-              msg_start = await conv.send_message("/start")
-              response = await conv.get_response()
-              r = await conv.get_response()
-              msg = await conv.send_message(d_link)
-              details = await conv.get_response()
-              song = await conv.get_response()
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
-          except YouBlockedUserError:
-              await Deezlod.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
-              return
-          await bot.send_file(Deezlod.chat_id, song, caption=details.text)
-          await Deezlod.client.delete_messages(conv.chat_id,
-                                             [msg_start.id, response.id, r.id, msg.id, details.id, song.id])
-          await Deezlod.delete()          
+
     
 CMD_HELP.update({
-        "music":
-        ".spd`<Artist - Song Title>\
-            \nUsage:For searching songs from Spotify.\
-            \n\n`.netease` <Artist - Song Title>\
-            \nUsage:Download music with @WooMaiBot\
-            \n\n`.dzd` <Spotify/Deezer Link>\
-            \nUsage:Download music from Spotify or Deezer.\
-            \n\n`.deezload` <spotify/deezer link> <Format>\
-            \nUsage: Download music from deezer.\
-            \n\n Well deezer is not available in India so create an deezer account using vpn. Set DEEZER_ARL_TOKEN in vars to make this work.\
-            \n\n *Format= `FLAC`, `MP3_320`, `MP3_256`, `MP3_128`.\
-            \n\n\n Guide:Video guide of arl token: [here](https://www.youtube.com/watch?v=O6PRT47_yds&feature=youtu.be) or Read [This](https://notabug.org/RemixDevs/DeezloaderRemix/wiki/Login+via+userToken)."
+        "gaana":
+        "`.netease` <Artist - Song Title>\
+            \nUsage:Download music with @WooMaiBot\"
 })
 
 
