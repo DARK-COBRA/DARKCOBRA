cd286a1a582b1320cbe5d8af35fe1f739c958c89
MHD SALIM <70217381+A-4-APPLE@users.noreply.github.com>
2020-09-04 12:31:35 +0000

    changed to spotdl

--- userbot/plugins/songs.py
+++ userbot/plugins/songs.py
@@ -1,4 +1,3 @@
-
 from telethon import events
 import subprocess
 from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
@@ -8,19 +7,8 @@ import time
 from userbot.utils import admin_cmd
 import glob
 import os
-try:
- import instantmusic , subprocess
-except:
- os.system("pip install instantmusic")
- 
-
-
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
-    
-    os.system("instantmusic -q -s "+name)
+import spotdl
+import subprocess
     
 
 @borg.on(admin_cmd(pattern="song ?(.*)"))
@@ -30,22 +18,24 @@ async def _(event):
     DELAY_BETWEEN_EDITS = 0.3
     PROCESS_RUN_TIME = 100
     cmd = event.pattern_match.group(1)
+    cmnd = f"{cmd}"
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("`i am going to find your song..🎶`"),
-    bruh(str(cmd))
+    subprocess.run(["spotdl", "-s", cmnd, "-q", "best"])
+    subprocess.run(
+        'for f in *.opus; do      mv -- "$f" "${f%.opus}.mp3"; done',
+        shell=True)
     l = glob.glob("*.mp3")
     loa = l[0]
-    await event.edit("`yeeeaah..! i found something..🎉`")
-    await borg.send_file(
-                event.chat_id,
-                loa,
-                force_document=True,
-                allow_cache=False,
-                caption="`📁File successfully Uploaded",
-                reply_to=reply_to_id
-            )
-    await event.delete()
-    os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3",shell=True)
+    await event.edit("`Yes 🌚 I found the song..🎶`")
+    await event.client.send_file(
+        event.chat_id,
+        loa,
+        force_document=False,
+        allow_cache=False,
+        supports_streaming=True,
+        caption=cmd,
+        reply_to=reply_to_id,
+    )
+    subprocess.run("rm -rf *.mp3", shell=True)
