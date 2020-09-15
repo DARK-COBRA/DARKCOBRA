d88ed67063d070f1af9d11dde134f451aeb17e19
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-09-15 08:16:39 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,16 +1,6 @@
 from userbot import darkdef
 
 
-
-from userbot import darkdef
-
-deEmojify = darkdef.deEmojify
-trumptweet = darkdef.trumptweet 
-changemymind = darkdef.changemymind
-kannagen = darkdef.kannagen
-moditweet = darkdef.moditweet
-tweets = darkdef.tweets
-waifutxt = darkdef.waifutxt
 darkmusic = darkdef.darkmusic 
 darkmusicvideo = darkdef.darkmusicvideo
-admin_groups = darkdef.admin_groups
+
