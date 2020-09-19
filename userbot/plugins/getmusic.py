cb3f7cd48542a54fa648ecab23d2c2e4d22ce4a3
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-09-19 14:14:41 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -23,17 +23,17 @@ def bruh(name):
     
     os.system("instantmusic -q -s "+name)
     
-@register(outgoing=True, pattern="^.sung(?: |$)(.*)")
+@register(outgoing=True, pattern="^.so(?: |$)(.*)")
 async def getmusic(s):
     if sung.fwd_from:
         return
-    song = sung.pattern_match.group(1)
+    song = so.pattern_match.group(1)
     chat = "@Lucy_robot"
     link = f"/song {song}"
-    await sung.edit("```Getting Your Music```")
+    await so.edit("```Getting Your Music```")
     async with bot.conversation(chat) as conv:
           await asyncio.sleep(2)
-          await sung.edit("`Downloading...Please wait`")
+          await so.edit("`Downloading...Please wait`")
           try:
               msg = await conv.send_message(link)
               response = await conv.get_response()
@@ -41,11 +41,11 @@ async def getmusic(s):
               """ - don't spam notif - """
               await bot.send_read_acknowledge(conv.chat_id)
           except YouBlockedUserError:
-              await sung.edit("```Please unblock @Lucy_robot and try again```")
+              await so.edit("```Please unblock @Lucy_robot and try again```")
               return
-          await sung.edit("`Sending Your Music...weit!😎`")
+          await so.edit("`Sending Your Music...weit!😎`")
           await asyncio.sleep(3)
           await bot.send_file(s.chat_id, respond)
-    await sung.client.delete_messages(conv.chat_id,
+    await so.client.delete_messages(conv.chat_id,
                                        [msg.id, response.id, respond.id])
-    await sung.delete()
+    await so.delete()
