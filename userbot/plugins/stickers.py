4994867edfd6052a288e9e09b4f5e1175c0b5af4
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-09-25 09:32:34 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -80,7 +80,7 @@ async def kang(args):
     if photo:
         splat = args.text.split()
         if not emojibypass:
-            emoji = "😂"
+            emoji = "❤"
         pack = 1
         if len(splat) == 3:
             pack = splat[2]  # User sent both
