14b5338482969e79323b954c29f57bb0a8ac991c
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-09-26 16:19:11 +0530

    Update lysm.py

--- userbot/plugins/lysm.py
+++ userbot/plugins/lysm.py
@@ -24,4 +24,30 @@ async def _(event):
         await asyncio.sleep(1)
         await event.edit("`Yar patta hai I love you so much❤`")
 
+        @borg.on(events.NewMessage(pattern=r"\.nehi", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    
+        await event.edit("ɪƒ ƴσυ'ʀє αʟσηє")
+        await asyncio.sleep(20)
+        await event.edit("ɪ'ʟʟ ɓє ƴσυʀ ѕɧα∂σѡ.")
+        await asyncio.sleep(20)
+        await event.edit(" ɪƒ ƴσυ ѡαηт тσ cʀƴ")
+        await asyncio.sleep(20)
+        await event.edit("ɪ'ʟʟ ɓє ƴσυʀ ѕɧσυʟ∂єʀ")
+        await asyncio.sleep(20)
+        await event.edit("ɪƒ ƴσυ ѡαηт α ɧυɢ")
+        await asyncio.sleep(20)
+        await event.edit("ɪ'ʟʟ ɓє ƴσυʀ ρɪʟʟσѡ")
+        await asyncio.sleep(20)
+        await event.edit("ɪƒ ƴσυ ηєє∂ тσ ɓє ɧαρρƴ")
+        await asyncio.sleep(20)
+        await event.edit("ɪ'ʟʟ ɓє ƴσυʀ ѕɱɪʟє")
+        await asyncio.sleep(15)
+        await event.edit("`ɓυт αηƴтɪɱє ƴσυ ηєє∂ αnчσnє, ɪt'ʟʟ ʝυѕт ɓє ɱє.❤`")
+        
+         return
+
+        await asyncio.sleep(999)
     
