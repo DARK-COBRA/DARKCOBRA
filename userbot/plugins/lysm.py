8f7d95d78ba866b5b24bcdd19eeee001670e6047
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-09-26 15:47:12 +0530

    Create lysm.py

---
+++ userbot/plugins/lysm.py
@@ -0,0 +1,27 @@
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="degi ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("Yar")
+        await asyncio.sleep(0.7)
+        await event.edit("Patta hai")
+        await asyncio.sleep(1)
+        await event.edit("I")
+        await asyncio.sleep(0.8)
+        await event.edit("Love")
+        await asyncio.sleep(0.9)
+        await event.edit("You")
+        await asyncio.sleep(1)
+        await event.edit("Sooooo")
+        await asyncio.sleep(0.8)
+        await event.edit("much")
+        await asyncio.sleep(0.9)
+        await event.edit("❤")
+        await asyncio.sleep(1)
+        await event.edit("`Yar patta hai I love you so much❤`")
+
+    await asyncio.sleep(999)
