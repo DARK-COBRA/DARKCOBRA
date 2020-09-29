606346e8ed43ddd82d5c2ff3c1a9662bbd522973
Praveen <70108597+Godzilla365@users.noreply.github.com>
2020-09-29 23:33:03 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,31 +5,27 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/76df9853cef5f5a857896.jpg"
-pm_caption = "**⚜ɢᴏᴅᴢɪʟʟᴀ ɪs ᴏɴʟɪɴᴇ⚜**\n"
- 
-pm_caption += f"**M̴y̴ ̴B̴o̴s̴s̴**       : {DEFAULTUSER}\n"
 
+pm_caption = "⚜ɢᴏᴅᴢɪʟʟᴀ ɪs ᴏɴʟɪɴᴇ⚜\n"
 
-pm_caption += "Mʏ Bᴏᴛ Sᴛᴀᴛᴜꜱ      : Wᴏʀᴋɪɴɢ ᴘᴇʀғᴇᴄᴛʟʏ🔥"    
+pm_caption += f"M Y   B O S S             : {DEFAULTUSER}\n"
 
+pm_caption += "Mʏ Bᴏᴛ Sᴛᴀᴛᴜꜱ : Wᴏʀᴋɪɴɢ ᴘᴇʀғᴇᴄᴛʟʏ 🔥\n"
 
-pm_caption += "тєℓєтнσи           : тєℓєтнσи-15.0.0 𖤐⃟🔷"           
+pm_caption += "тєℓєтнσи     : тєℓєтнσи-15.0.0 𖤐⃟🔷\n"
 
-pm_caption += "ρутнσи             : ρутнσи-3.8.5 𖤐⃟🔷"                 
+pm_caption += "ρутнσи       : ρутнσи-3.8.5 𖤐⃟🔷\n"
 
+pm_caption += "ι'ℓℓ вє ωιтн му мαѕтєя тιℓℓ му ∂уиσ єи∂ѕ!!☠ 𖤐⃟🛰\n"
 
+pm_caption += "ραят σғ тнε נσυяηεү ιs тнε εη∂🖋\n"
 
+pm_caption += "[┏┓━┏┓━━━━┏┓━┏┓━━━━━\n ┃┃━┃┃━━━━┃┃━┃┃━━━━━\n ┃┗━┛┃┏━━┓┃┃━┃┃━┏━━┓\n ┃┏━┓┃┃┏┓┃┃┃━┃┃━┃┏┓┃ \n ┃┃━┃┃┃┃━┫┃┗┓┃┗┓┃┗┛┃ \n ┗┛━┗┛┗━━┛┗━┛┗━┛┗━━┛](https://t.me/Mothra365)"
 
-pm_caption += "My Master's other acc  : @Godzilla_365 🔱"       
-
-
-
-pm_caption += "ι'ℓℓ вє ωιтн му мαѕтєя тιℓℓ му ∂уиσ єи∂ѕ!!☠ 𖤐⃟🛰"                                            
-
-
-
-pm_caption += " [┏┓━┏┓━━━━┏┓━┏┓━━━━━\n ┃┃━┃┃━━━━┃┃━┃┃━━━━━\n ┃┗━┛┃┏━━┓┃┃━┃┃━┏━━┓\n ┃┏━┓┃┃┏┓┃┃┃━┃┃━┃┏┓┃ \n ┃┃━┃┃┃┃━┫┃┗┓┃┗┓┃┗┛┃ \n ┗┛━┗┛┗━━┛┗━┛┗━┛┗━━┛](https://t.me/Mothra365)"
 #@command(outgoing=True, pattern="^.alive$")
+
+
+
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     chat = await alive.get_chat()
