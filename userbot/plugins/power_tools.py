372a430d1da849e891f4eccf14fe08350d6b5dba
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-10-09 08:04:58 +0530

    Update power_tools.py

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -16,9 +16,9 @@ from uniborg.util import admin_cmd
 async def _(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
      await asyncio.sleep(2)
-     await event.edit("Restarting [██░] ...\n)
+     await event.edit("Restarting [██░] ...)
      await asyncio.sleep(2)
-     await event.edit("Restarting [███]...\n)
+     await event.edit("Restarting [███]......)
      await asyncio.sleep(2)
     await event.edit("Restarted boss....✈ `.king` me or type `.help` or type '.awake' or type '.allive' to check if your DC is online/alive ")
     await borg.disconnect()
