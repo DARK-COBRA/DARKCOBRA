0ab834ced760a9c3595e23a44a42f2a3c2101e64
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-10-09 07:38:59 +0530

    Update qbot.py

--- userbot/plugins/qbot.py
+++ userbot/plugins/qbot.py
@@ -9,15 +9,16 @@ from userbot.utils import admin_cmd
 
 #@register(outgoing=True, pattern="^.q(?: |$)(.*)")
 @borg.on(admin_cmd(pattern=r"qbot(?: |$)(.*)"))
+@borg.on(events.NewMessage(pattern=r"\.qbot(.*)",incoming=True))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
+       await event.reply("```Reply to any user message.```")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit("```Reply to text message```")
+       await event.reply("```Reply to text message```")
        return
     chat = "@QuotLyBot"
     sender = reply_message.sender
