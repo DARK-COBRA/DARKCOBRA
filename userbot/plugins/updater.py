968606e6c5f16f31c056d25c1ff2134b265526a1
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-10-11 07:43:26 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -20,7 +20,7 @@ requirements_path = path.join(
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 GIT_REPO_NAME = "DARKCOBRA"
-UPSTREAM_REPO_URL = "https://github.com/DARK-COBRA/DARKCOBRA"
+UPSTREAM_REPO_URL = "https://github.com/DARK-COBRA/DARKCOBRA.git"
 
 async def gen_chlog(repo, diff):
     ch_log = ''
