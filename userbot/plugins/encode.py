5bc28095e586139a3c82a8352966e8ff43d74ced
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-10-20 10:02:01 +0530

    Update encode.py

--- userbot/plugins/encode.py
+++ userbot/plugins/encode.py
@@ -5,8 +5,9 @@
 import pygments, os, asyncio
 from pygments.lexers import Python3Lexer
 from pygments.formatters import ImageFormatter
+from userbot.utils import admin_cmd
 
-@register(outgoing=True, pattern="^.encode(?: |$)(.*)")
+@borg.on(admin_cmd(pattern="encode ?(.*)"))
 async def coder_print(event):
 	a = await event.client.download_media(await event.get_reply_message(), Var.TEMP_DOWNLOAD_DIRECTORY)
 	s = open(a, 'r')
