bca15f52da483544ffa19fe4c634d2144d1a43d0
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-10-21 10:28:13 +0530

    Huh..

---
+++ userbot/plugins/cmds.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from userbot.utils import admin_cmd
+
+
+@borg.on(admin_cmd(pattern="cmds", outgoing=True))
+async def install(event):
+    if event.fwd_from:
+        return
+    cmd = "ls userbot/plugins"
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    o = stdout.decode()
+    _o = o.split("\n")
+    o = "\n".join(_o)
+    OUTPUT = f"**List of Plugins:**\n`{o}`\n\n**TIP:** __If you want to know the commands for a plugin, do:-__ \n `.help <plugin name>` **without the < > brackets.**\n__All plugins might not work directly.\n Visit @Dark_cobra_support_group for assistance..."
+    await event.edit(OUTPUT)
