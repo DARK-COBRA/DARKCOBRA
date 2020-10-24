85018124fb8b2cc8e7600730397e8abac36b6208
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-10-24 15:39:02 +0530

    Nsfw...

---
+++ userbot/plugins/nsfw.py
@@ -0,0 +1,34 @@
+# Ported from userge by @buddhhu
+
+import os, urllib, requests, asyncio
+
+
+@plus_ub(pattern=r"boobs", from_users=sudo)
+async def boobs(event):
+    if not os.path.isdir(Var.TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Var.TEMP_DOWNLOAD_DIRECTORY)
+    pic_loc = os.path.join(Var.TEMP_DOWNLOAD_DIRECTORY, "bobs.jpg")
+    a = await event.reply("`Finding some big bobs 🧐...`")
+    await asyncio.sleep(0.5)
+    await a.edit("`Sending some big bobs 🌚...`")
+    nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), pic_loc)
+    await event.client.send_file(event.chat_id, pic_loc, force_document=False)
+    os.remove(pic_loc)
+    await event.delete()
+    await a.delete()
+
+@plus_ub(pattern=r"butts", from_users=sudo)
+async def butts(event):
+    if not os.path.isdir(Var.TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Var.TEMP_DOWNLOAD_DIRECTORY)
+    pic_loc = os.path.join(Var.TEMP_DOWNLOAD_DIRECTORY, "butts.jpg")
+    a = await event.reply("`Finding some beautiful butts 🧐...`")
+    await asyncio.sleep(0.5)
+    await a.edit("`Sending some beautiful butts 🌚...`")
+    nsfw = requests.get('http://api.obutts.ru/noise/1').json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), pic_loc)
+    await event.client.send_file(event.chat_id, pic_loc, force_document=False)
+    os.remove(pic_loc)
+    await event.delete()
+    await a.delete()
