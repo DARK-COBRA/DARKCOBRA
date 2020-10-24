6768ae35620fbdcb7964f6b6e656f9547c674d2d
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-10-24 15:56:54 +0530

    🤥

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,6 +1,6 @@
 import asyncio
 from telethon import events
-from uniborg.util import admin_cmd
+from userbot.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "DARK COBRA"
