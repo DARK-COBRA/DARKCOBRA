399c090f5679d8935241a35777088a6844f49ef4
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-10-30 21:01:50 +0530

    ``

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -57,6 +57,14 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
+    async def on_plug_in_callback_query_handler(event):
+        if event.query.user_id == bot.uid:
+            await event.edit("The menu has been closed..!")
+
+
+
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
     ))
