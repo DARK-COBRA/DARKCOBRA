c2a52d1bdc0b2c9e687c7c940e9cb4e687bebb22
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-02 18:32:23 +0530

    Create Spotify.py

---
+++ userbot/plugins/Spotify.py
@@ -0,0 +1,12 @@
+from telethon.tl.types import InputMessagesFilterMusic
+
+register(outgoing=True, pattern="^.sptfy(?: |$)(.*)")
+async def spotify(so):
+    if so.fwd_from:
+        return
+id = event.chat_id
+chat = -1001271479322
+song = f"{song}"
+async for event in event.client.iter_messages(chat, search=song, limit=1, filter=InputMessagesFilterMusic):
+    lol = event
+    await event.client.send_file(id, lol, caption=event.message)
