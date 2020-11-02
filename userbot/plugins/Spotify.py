fcb06ea0928760ddfa091d81f921c14ce9d31969
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-02 18:43:22 +0530

    Update Spotify.py

--- userbot/plugins/Spotify.py
+++ userbot/plugins/Spotify.py
@@ -2,9 +2,7 @@ from telethon.tl.types import InputMessagesFilterMusic
 from userbot.events import register
 
 @register(outgoing=True, pattern="^.sptfy(?: |$)(.*)")
-async def spotify(so):
-    if so.fwd_from:
-        return
+
 id = event.chat_id
 chat = -1001271479322
 song = f"{song}"
