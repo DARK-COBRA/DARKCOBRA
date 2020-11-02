0ca32f3d56cb5568900926761aa273320b772020
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-02 20:50:56 +0530

    Hmm..

--- userbot/plugins/getsongs.py
+++ userbot/plugins/getsongs.py
@@ -10,7 +10,7 @@ import os
 import spotdl
 import subprocess
 
-@borg.on(admin_cmd(pattern="song ?(.*)"))
+@borg.on(admin_cmd(pattern="getsong ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
