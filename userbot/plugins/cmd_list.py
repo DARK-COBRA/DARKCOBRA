1bddc113fb3546144dd25cfff832ee1f81e44a75
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-09 08:07:57 +0530

    Ok

--- userbot/plugins/cmd_list.py
+++ userbot/plugins/cmd_list.py
@@ -3,6 +3,7 @@ import subprocess
 import asyncio
 import time
 from userbot.utils import admin_cmd
+from userbot import CMD_HELP
 
 #@command(pattern="^.cmds", outgoing=True)
 @borg.on(admin_cmd(pattern=r"cmds"))
@@ -17,5 +18,18 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"**List of Plugins:**\n{o}\n\n**HELP:** __If you want to know the commands for a plugin, do:-__ \n `.help <plugin name>` **without the < > brackets.**\n__All plugins might not work directly. Visit__ @No_OnE_Kn0wS_Me__for assistance.__"
-    await event.edit(OUTPUT)
+    OUTPUT = (
+        OUTPUT
+    ) = f"**List of Plugins:**\n{o}\n\n**HELP:** __If you want to know the commands for a plugin, do:-__ \n `.help <plugin name>` **without the < > brackets.**\n__All plugins might not work directly. Visit__ @Dark_cobra_support_group__for assistance.__"
+    await event.edit("`Plugins extracted, pasting it weit a second...`")
+    message = OUTPUT
+    url = "https://nekobin.com/documents"
+    r = requests.post(url, data=message.encode("UTF-8")).json()
+    url = f"https://nekobin.com/{r['key']}"
+    await event.edit(
+        f"`All plugins have been successfully extracted and can be found [here]({url})!!"
+    )
+
+CMD_HELP.update(
+    {"command_list": ".cmds\nUsage - Extracts all the plugins of this userbot in a link.."}
+)
