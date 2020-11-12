cbf197a916f69a3e407ed6c6379a65b802f86835
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-12 11:07:41 +0530

    Update power_tools.py

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -1,7 +1,8 @@
 """Restart or Terminate the bot from any chat
 Available Commands:
-.restartsys
-.shutdown"""
+.restart
+.shutdown
+.reboot"""
 # This Source Code Form is subject to the terms of the GNU
 # General Public License, v.3.0. If a copy of the GPL was not distributed with this
 # file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html
@@ -14,10 +15,12 @@ from uniborg.util import admin_cmd
 
 @borg.on(admin_cmd(pattern="restart"))
 async def _(event):
-    await event.edit("Restarting [██░] ...")
-    await asyncio.sleep(2)
-    await event.edit("Restarting [███]...")
-    await asyncio.sleep(2)
+    await event.edit("Restarting ▰▱▱▱▱▱▱▱18%...")
+    await asyncio.sleep(1)
+    await event.edit("Restarting ▰▰▰▰▱▱▱▱49.6%...")
+    await asyncio.sleep(1.5)
+    await event.edit("Restarting ▰▰▰▰▰▰▰▰100%...")
+    await asyncio.sleep(3)
     await event.edit("Restarted boss....✈ `.king` me or type `.help` or type '.awake' or type '.allive' to check if your DC is online/alive ")
     await borg.disconnect()
     os.execl(sys.executable, sys.executable, *sys.argv)
@@ -31,3 +34,17 @@ async def _(event):
         return
     await event.edit("Turning dyno off ...Manually turn me on later")
     await borg.disconnect()
+
+@borg.on(events.NewMessage(pattern=r"\.reboot", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    
+    await event.edit("╭━━━╮\n┃╭━╮┃\n┃╰━━┳━━┳━┳╮╭┳━━┳━╮\n╰━━╮┃┃━┫╭┫╰╯┃┃━┫╭╯\n┃╰━╯┃┃━┫┃╰╮╭┫┃━┫┃\n╰━━━┻━━┻╯╱╰╯╰━━┻╯\n╭━━━╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮\n┃╭━╮┃╱╱╱╱╭╯╰╮╱╱╱╭╯╰╮\n┃╰━╯┣━━┳━┻╮╭╋━━┳┻╮╭╋┳━╮╭━━╮\n┃╭╮╭┫┃━┫━━┫┃┃╭╮┃╭┫┃┣┫╭╮┫╭╮┃\n┃┃┃╰┫┃━╋━━┃╰┫╭╮┃┃┃╰┫┃┃┃┃╰╯┣┳┳╮\n╰╯╰━┻━━┻━━┻━┻╯╰┻╯╰━┻┻╯╰┻━╮┣┻┻╯\n╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃\n╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯")
+    await asyncio.sleep(2)
+    await event.edit("╭━━━╮\n┃╭━╮┃\n┃╰━━┳━━┳━┳╮╭┳━━┳━╮\n╰━━╮┃┃━┫╭┫╰╯┃┃━┫╭╯\n┃╰━╯┃┃━┫┃╰╮╭┫┃━┫┃\n╰━━━┻━━┻╯╱╰╯╰━━┻╯\n╭━━━╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮\n┃╭━╮┃╱╱╱╱╭╯╰╮╱╱╱╭╯╰╮╱╱╱╱┃┃\n┃╰━╯┣━━┳━┻╮╭╋━━┳┻╮╭╋━━┳━╯┃\n┃╭╮╭┫┃━┫━━┫┃┃╭╮┃╭┫┃┃┃━┫╭╮┃j\n┃┃┃╰┫┃━╋━━┃╰┫╭╮┃┃┃╰┫┃━┫╰╯┣╮\n╰╯╰━┻━━┻━━┻━┻╯╰┻╯╰━┻━━┻━━┻╯")
+    await asyncio.sleep(0.1)
+    await event.edit("🇸 🇪 🇷 🇻 🇪 🇷  🇷 🇪 🇧 🇴 🇴 🇹 🇪 🇩  = ✅")
+    await borg.disconnect()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
