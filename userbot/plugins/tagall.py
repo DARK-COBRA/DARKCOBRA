b4eaa481facb83186168d7c60eb1f090478fc5ea
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-23 12:51:11 +0530

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -1,29 +1,31 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2
-import asyncio
-from telethon import events
+# HEHEHEHEHEHHEHEHHEEHEHHE
+
+
 from telethon.tl.types import ChannelParticipantsAdmins
 from userbot.utils import admin_cmd
-
+from userbot import CMD_HELP
 
 @borg.on(admin_cmd(pattern=r"tagall", outgoing=True))
+@borg.on(admin_cmd(pattern=r"tagall", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "Hello"
+    mentions = "Check that msg 🙂🙂 "
     chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, 100):
+    async for x in borg.iter_participants(chat, 111):
         mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    await event.reply(mentions)
-    await event.delete()
+    await event.edit(mentions)
+   
 
 
-@borg.on(admin_cmd(pattern=r"administrator", outgoing=True))
+@borg.on(admin_cmd(pattern=r"admin", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "Administrators : "
+    mentions = "Admins : "
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
@@ -32,5 +34,15 @@ async def _(event):
         reply_message = await event.get_reply_message()
         await reply_message.reply(mentions)
     else:
-        await event.reply(mentions)
-    await event.delete()
+        await event.edit(mentions)
+    
+
+CMD_HELP.update(
+    {
+        "tagall": ".tagall\
+    \nReplay any msg with .tagall nd u'll tag top 111 active mem of a grp.\
+    \n\n.admin\
+    \nSame as above but mention only admins."
+  }
+
+)
