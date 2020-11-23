ef29b4c45f204c3d237ab5b481e9974357240c26
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-23 10:25:48 +0530

    Update and rename whatscrap.py to copy.py

--- userbot/plugins/copy.py
+++ userbot/plugins/copy.py
@@ -1,9 +1,9 @@
-"""Syntax: .whatscrapp as reply to a message copied from @WhatsCRApp"""
+
 from telethon import events
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="whatscrapp"))
+@borg.on(admin_cmd(pattern="copy"))
 async def _(event):
     if event.fwd_from:
         return
@@ -11,8 +11,8 @@ async def _(event):
         previous_message = await event.get_reply_message()
         the_real_message = previous_message.text
         reply_to_id = event.reply_to_msg_id
-        the_real_message = the_real_message.replace("*", "**")
-        the_real_message = the_real_message.replace("_", "__")
+        the_real_message = the_real_message.replace("*", "*")
+        the_real_message = the_real_message.replace("_", "_")
         await event.edit(the_real_message)
     else:
-        await event.edit("Reply to a message with `.whatscrapp` to format @WhatsCRApp messages to @Telegram")
+        await event.edit("Reply to a  message .copy to copy nd paste ")
