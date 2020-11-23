a9254d6a7e3e5bc62b9d44123bb3cf463b712775
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-24 00:45:41 +0530

    Update tweet.py

--- userbot/tweet.py
+++ userbot/tweet.py
@@ -49,31 +49,3 @@ async def johnnytweet(text):
     img.save("hehe.webp", "webp")
     return "hehe.webp"
 
-async def kimtweet(text):
-    r = requests.get(
-        f"https://nekobot.xyz/api/imagegen?type=tweet&text={text}&username=_Kim_Jongun"
-    ).json()
-    dc = r.get("message")
-    cobra = url(dc)
-    if not cobra:
-        return "check syntax once more"
-    with open("hehe.png", "wb") as f:
-        f.write(requests.get(dc).content)
-    img = Image.open("hehe.png").convert("RGB")
-    img.save("hehe.webp", "webp")
-    return "hehe.webp"
-
-async def bhautweet(text):
-    r = requests.get(
-        f"https://nekobot.xyz/api/imagegen?type=tweet&text={text}&username=hindustanibhau"
-    ).json()
-    dc = r.get("message")
-    cobra = url(dc)
-    if not cobra:
-        return "check syntax once more"
-    with open("hehe.png", "wb") as f:
-        f.write(requests.get(dc).content)
-    img = Image.open("hehe.png").convert("RGB")
-    img.save("hehe.webp", "webp")
-    return "hehe.webp"
-
