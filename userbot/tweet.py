9a789e588ecfae5587a885f7e7aadb00736b20d5
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-23 22:20:35 +0530

    Create tweet.py

---
+++ userbot/tweet.py
@@ -0,0 +1,50 @@
+import os
+import re
+import urllib.request
+import PIL.ImageOps
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from validators.url import url
+
+async def moditweet(text):
+    r = requests.get(
+        f"https://nekobot.xyz/api/imagegen?type=tweet&text={text}&username=narendramodi"
+    ).json()
+    dc = r.get("message")
+    cobra = url(dc)
+    if not cobra:
+        return "check syntax once more"
+    with open("hehe.png", "wb") as f:
+        f.write(requests.get(dc).content)
+    img = Image.open("hehe.png").convert("RGB")
+    img.save("hehe.webp", "webp")
+    return "hehe.webp"
+
+
+async def sunnytweet(text):
+    r = requests.get(
+        f"https://nekobot.xyz/api/imagegen?type=tweet&text={text}&username=narendramodi"
+    ).json()
+    dc = r.get("message")
+    cobra = url(dc)
+    if not cobra:
+        return "check syntax once more"
+    with open("hehe.png", "wb") as f:
+        f.write(requests.get(dc).content)
+    img = Image.open("hehe.png").convert("RGB")
+    img.save("hehe.webp", "webp")
+    return "hehe.webp"
+
+async def johnnytweet(text):
+    r = requests.get(
+        f"https://nekobot.xyz/api/imagegen?type=tweet&text={text}&username=narendramodi"
+    ).json()
+    dc = r.get("message")
+    cobra = url(dc)
+    if not cobra:
+        return "check syntax once more"
+    with open("hehe.png", "wb") as f:
+        f.write(requests.get(dc).content)
+    img = Image.open("hehe.png").convert("RGB")
+    img.save("hehe.webp", "webp")
+    return "hehe.webp"
