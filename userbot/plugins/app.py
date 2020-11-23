9a153365d05555a1519dceb6bd36e5d5597e46bb
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-23 15:46:11 +0530

    Update app.py

--- userbot/plugins/app.py
+++ userbot/plugins/app.py
@@ -1,27 +1,24 @@
-"""Fetch App Details from Playstore.
-.app <app_name> to fetch app details.
-.appr <app_name>  to fetch app details with Xpl0iter request link."""
-
-import requests
-
-import bs4
-
 import re
+import bs4
+import requests
+from platform import uname
+from telethon import events
+from bs4 import BeautifulSoup
+from telegraph import Telegraph
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 
+from userbot import Var
 
+from userbot import CMD_HELP ,ALIVE_NAME
+from userbot.utils import admin_cmd
 
-from telethon import *
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
-from userbot import CMD_HELP
-
-from userbot.events import register
-
-
-@borg.on(events.NewMessage(pattern='.app (.*)'))
-
-async def apk(e):
+@borg.on(admin_cmd(pattern=r"app (.*)"))
+async def apk(event):
+    app_name = event.pattern_match.group(1)
+    event = await event.edit("Searching!")
     try:
-        app_name = e.pattern_match.group(1)
         remove_space = app_name.split(' ')
         final_name = '+'.join(remove_space)
         page = requests.get("https://play.google.com/store/search?q="+final_name+"&c=apps")
@@ -39,41 +36,29 @@ async def apk(e):
         app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
         app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
         app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
-        app_details += "\n\n===> DARK COBRA <==="
-        await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
+        app_details += f"\n\n===> @Dark_cobra_support <==="
+        await event.edit(app_details, link_preview = True, parse_mode = 'HTML')
     except IndexError:
-        await e.edit("No result found in search. Please enter **Valid app name**")
+        await event.edit("No result found in search. Please enter **Valid app name**")
     except Exception as err:
-        await e.edit("Exception Occured:- "+str(err))
+        await event.edit("Exception Occured:- "+str(err))
 
-@borg.on(events.NewMessage(pattern='.appr (.*)'))
 
-@borg.on(events.MessageEdited(pattern='.appr (.*)'))
 
-async def apkr(e):
-    try:
-        app_name = e.pattern_match.group(1)
-        remove_space = app_name.split(' ')
-        final_name = '+'.join(remove_space)
-        page = requests.get("https://play.google.com/store/search?q="+final_name+"&c=apps")
-        lnk = str(page.status_code)
-        soup = bs4.BeautifulSoup(page.content,'lxml', from_encoding='utf-8')
-        results = soup.findAll("div","ZmHEEd")
-        app_name = results[0].findNext('div', 'Vpfmgd').findNext('div', 'WsMG1c nnK0zc').text
-        app_dev = results[0].findNext('div', 'Vpfmgd').findNext('div', 'KoLSrc').text
-        app_dev_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('a', 'mnKHRc')['href']
-        app_rating = results[0].findNext('div', 'Vpfmgd').findNext('div', 'pf5lIe').find('div')['aria-label']
-        app_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('div', 'vU6FJ p63iDd').a['href']
-        app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
-        app_details = "<a href='"+app_icon+"'>📲&#8203;</a>"
-        app_details += " <b>"+app_name+"</b>"
-        app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
-        app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
-        app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
-        app_details += "\n\n<b>Download : </b> <a href='https://t.me/joinchat/JCu-H1NikiYDgNjpjPYd4A'>Request_Here</a>"
-        app_details += "\n\n===> DARK COBRA <==="
-        await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
-    except IndexError:
-        await e.edit("No result found in search. Please enter **Valid app name**")
-    except Exception as err:
-        await e.edit("Exception Occured:- "+str(err))
+@bot.on(admin_cmd(pattern="appx ?(.*)"))
+async def mod(event):
+    if event.fwd_from:
+        return
+    modr = event.pattern_match.group(1)
+    botusername = "@PremiumAppBot"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, modr)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+CMD_HELP.update(
+    {
+        "app": "`.app (app name)\nUse - Get a preview nd link of all apps from playstore.\
+        \n\n.appx (app name)\\nUse - send mod apks file.`"})
+        
