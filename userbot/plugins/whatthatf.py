88a67403c08a62747844c1d31d2997bf00053791
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-23 10:25:24 +0530

    Fixed ig

--- userbot/plugins/whatthatf.py
+++ userbot/plugins/whatthatf.py
@@ -6,7 +6,7 @@ from telethon import events, functions, __version__
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="f"))
+@borg.on(admin_cmd(pattern="payf ?(.*)"))
 async def payf(event):
     paytext=event.pattern_match.group(1)
     pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(paytext*8, paytext*8, paytext*2, paytext*2, paytext*2, paytext*6, paytext*6, paytext*2, paytext*2, paytext*2, paytext*2, paytext*2)
