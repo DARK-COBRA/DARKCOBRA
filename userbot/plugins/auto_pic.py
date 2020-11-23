b5a6f54e37b5fbe85615c19c074ca622112a8a79
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-23 14:04:18 +0530

    Update auto_pic.py

--- userbot/plugins/auto_pic.py
+++ userbot/plugins/auto_pic.py
@@ -638,3 +638,44 @@ async def main(event):
         os.system("rm -rf donottouch.jpg")
 
         await asyncio.sleep(400)
+
+
+import requests , re , random 
+import urllib , os 
+from telethon.tl import functions
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from userbot.utils import admin_cmd
+import asyncio
+from time import sleep
+
+COLLECTION_STRING = [
+  "star-wars-wallpaper-1080p",
+  "4k-sci-fi-wallpaper",
+  "star-wars-iphone-6-wallpaper",
+  "kylo-ren-wallpaper",
+  "darth-vader-wallpaper"
+]
+
+async def gamepp():
+    os.system("rm -rf donot.jpg")
+    rnd = random.randint(0, len(COLLECTION_STRING) - 1)
+    pack = COLLECTION_STRING[rnd]
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+    f = re.compile('/\w+/full.+.jpg')
+    f = f.findall(pc)
+    fy = "http://getwallpapers.com"+random.choice(f)
+    print(fy)
+    if not os.path.exists("f.ttf"):
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+@borg.on(admin_cmd(pattern="gamerpfp ?(.*)"))
+async def main(event):
+    await event.edit("**Starting Gamer Profile Pic.**") #Owner @NihiNivi
+    while True:
+        await gamepp()
+        file = await event.client.upload_file("donottouch.jpg")  
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+        os.system("rm -rf donottouch.jpg")
+        await asyncio.sleep(400)
+
