33bbb9fd5c93358b54d2233947a9596ea1a5722a
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-24 17:09:28 +0530

    Update color.py

--- userbot/plugins/color.py
+++ userbot/plugins/color.py
@@ -8,11 +8,10 @@ import os, urllib, requests, asyncio
 from userbot.utils import admin_cmd
 from userbot import CMD_HELP
 
+DARKCOBRA = Config.DEEP_AI if Config.DEEP_AI else "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
 @borg.on(admin_cmd(pattern="colp$", outgoing=True))
 async def detect(event):
-    if Config.DEEP_AI is None:
-        return await event.edit(
-             "Add VAR DEEP_AI get Api Key from https://deepai.org or u can use .acolor without api")
+    
                
     reply = await event.get_reply_message()
     if not reply:#By @Danish_00
@@ -33,7 +32,7 @@ async def detect(event):
         files={
             "image": open(media, "rb"),
         },
-        headers={"api-key": Config.DEEP_AI},
+        headers={"api-key": DARKCOBRA},
     )#By @Danish_00
 #Fixed By a NOOB
     os.remove(media)
@@ -52,51 +51,10 @@ async def detect(event):
     )
 
 
-@borg.on(admin_cmd(pattern="acolor$", outgoing=True))
-async def _(event):
-          
-    reply = await event.get_reply_message()
-    if not reply:#By @Danish_00
-#Fixed By a NOOB
-        return await event.edit(
-           "Reply to any image or non animated sticker !"
-        )
-    devent = await event.edit("Downloading the file to check...")
-    media = await event.client.download_media(reply)
-    if not media.endswith(("png", "jpg", "webp")):
-        return await event.edit(
-             "Reply to any image or non animated sticker !"
-        )#By @Danish_00
-#Fixed By a NOOB
-    devent = await event.edit("coloring image sar...")
-    r = requests.post(
-        "https://api.deepai.org/api/colorizer",
-        files={
-            "image": open(media, "rb"),
-        },
-        headers={"api-key": quickstart-QUdJIGlzIGNvbWluZy4uLi4K},
-    )#By @Danish_00
-#Fixed By a NOOB
-    os.remove(media)
-    if "status" in r.json():
-        return await devent.edit( r.json()["status"])
-    r_json = r.json()["output_url"]
-    pic_id = r.json()["id"]
-    
-    link = f"https://api.deepai.org/job-view-file/{pic_id}/inputs/image.jpg"
-    result = f"{r_json}"
-    
-    await devent.delete()
-    await borg.send_message(
-        event.chat_id,
-        file=result
-    )
 
 CMD_HELP.update(
     {
         "color": 
     ".colp <reply to any black nd white media> "
-    "\nIt colorize the pic but u need API from deepai.org nd set var"
-    "\n\n.acolor <reply to any black nd white media> "
-    "\nIt colorize the pic without API but don't know how long it'll last 😅"
+    "\nIt colorize any black-and-white pic/sticker   `(Note :-if its not working then go to deepai.org then get api and set var DEEP_AI nd key.)`"
     })
