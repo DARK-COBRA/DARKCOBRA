363f68de59eb82c7e37ce89c487aa1fef1d9582f
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-25 09:37:35 +0530

    Update admin.py

--- userbot/plugins/admin.py
+++ userbot/plugins/admin.py
@@ -819,30 +819,30 @@ async def get_user_from_id(user, event):
 
 
 
-CMD_HELP.update({
-
- "admin": ".promote <username/reply> <custom rank (optional)>\
-\nUsage--Provides admin rights to the person in the chat.\
+      CMD_HELP.update({
+    "admin":
+    ".promote <username/reply> <custom rank (optional)>\
+\nUsage Provides admin rights to the person in the chat.\
 \n\n.demote <username/reply>\
 \nUsage Revokes the person's admin permissions in the chat.\
 \n\n.ban <username/reply> <reason (optional)>\
-\nUsage-- Bans the person off your chat.\
+\nUsage Bans the person off your chat.\
 \n\n.unban <username/reply>\
-\nUsage-- Removes the ban from the person in the chat.\
+\nUsage Removes the ban from the person in the chat.\
 \n\n.mute <username/reply> <reason (optional)>\
-\nUsage-- Mutes the person in the chat, works on admins too.\
+\nUsage Mutes the person in the chat, works on admins too.\
 \n\n.unmute <username/reply>\
-\nUsage-- Removes the person from the muted list.\
+\nUsage Removes the person from the muted list.\
 \n\n.gmute <username/reply> <reason (optional)>\
-\nUsage: Mutes the person in all groups you have in common with them.\
+\nUsage Mutes the person in all groups you have in common with them.\
 \n\n.ungmute <username/reply>\
-\nUsage-- Reply someone's message with .ungmute to remove them from the gmuted list.\
+\nUsage Reply someone's message with .ungmute to remove them from the gmuted list.\
 \n\n.delusers\
-\nUsage-- Searches for deleted accounts in a group. Use .delusers clean to remove deleted accounts from the group.\
+\nUsage Searches for deleted accounts in a group. Use .delusers clean to remove deleted accounts from the group.\
 \n\n.admins\
-\nUsage-- Retrieves a list of admins in the chat.\
+\nUsage Retrieves a list of admins in the chat.\
 \n\n.users or .users <name of member>\
-\nUsage-- Retrieves all (or queried) users in the chat.\
+\nUsage Retrieves all (or queried) users in the chat.\
 \n\n.setgppic <reply to image>\
-\nUsage-- Changes the group's display picture."
+\nUsage Changes the group's display picture."
 })
