8d684d1dcdefc77574f6e7bef77c645d98c6cc37
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-25 23:54:34 +0530

    okk

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from .. import ALIVE_NAME, CMD_HELP
 from ..utils import admin_cmd, edit_or_reply
-from userbot import sudo_cmd
+from userbot.utils import sudo_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "DARK COBRA"
 
