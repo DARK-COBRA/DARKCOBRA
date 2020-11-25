071484ea531974a524696f7b2c5db4fb8fc4c113
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-25 16:03:04 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -319,11 +319,11 @@ CMD_HELP.update(
     {
         "afk": "#AFK"
 "\n\n📌** CMD ➥** `.afk` [Optional Reason]"
-"\n**USAGE   ➥  **Sets you as afk.\nReplies to anyone who tags/PM's 
-you telling them that you are AFK(reason)\n\n__Switches off AFK when you type back anything, anywhere.__"
+"\n**USAGE   ➥  **Sets you as afk.\nReplies to anyone who tags/PM's" 
+"you telling them that you are AFK(reason)\n\n__Switches off AFK when you type back anything, anywhere.__"
 "\n\n📌** CMD ➥** `.mafk` [Optional Reason] (link of a media)\
-"\n**USAGE   ➥  **Sets you as afk(**WITH MEDIA**).\nReplies to anyone who tags/PM's 
-you telling them that you are AFK(reason)\n\n__Switches off AFK when you type back anything, anywhere.__"
+"\n**USAGE   ➥  **Sets you as afk(**WITH MEDIA**).\nReplies to anyone who tags/PM's" 
+"you telling them that you are AFK(reason)\n\n__Switches off AFK when you type back anything, anywhere.__"
 
     }
 )
