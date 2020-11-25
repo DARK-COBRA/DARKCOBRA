48a3bd50fa30b3592f7bc5667f156b4425d2af7e
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-25 15:01:59 +0530

    @noob import bhul gye sar

--- userbot/plugins/zombies.py
+++ userbot/plugins/zombies.py
@@ -1,18 +1,10 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-"""Cmd= `.zombie`
-Usage: Searches for deleted accounts in a groups and channels.
-Use .zombies clean to remove deleted accounts from the groups and channels.
-\nPorted by ©[NIKITA](t.me/kirito6969) and ©[EYEPATCH](t.me/NeoMatrix90)"""
 
 from telethon import events
 from userbot.utils import admin_cmd
 #
 from asyncio import sleep
 from os import remove
-
+from userbot import CMD_HELP
 from telethon.errors import (BadRequestError, ChatAdminRequiredError,
                              ImageProcessFailedError, PhotoCropSizeSmallError,
                              UserAdminInvalidError)
