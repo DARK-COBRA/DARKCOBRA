a4424b6a0a71bde1e89e4d7090ab7863c57de795
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-27 12:52:07 +0530

    Update acc_profile.py

--- userbot/plugins/acc_profile.py
+++ userbot/plugins/acc_profile.py
@@ -162,7 +162,5 @@ CMD_HELP.update({
 \n\n.delpfp or .delpfp <number>/<all>\
 \nUsage Deletes your Telegram profile picture(s).\
 \n\n.myusernames\
-\nUsage Shows usernames reserved by you.that is created by you channels or groups\
-\n\n.count\
-\nUsage Counts your groups, chats, bots etc..."
+\nUsage Shows usernames reserved by you.that is created by you channels or groups"
 })
