2c5872c4df6d886f7eaef1be438b02e27bd39cd0
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-27 09:04:10 +0530

    Hope

--- userbot/plugins/gps.py
+++ userbot/plugins/gps.py
@@ -3,6 +3,9 @@ from geopy.geocoders import Nominatim
 from userbot.utils import admin_cmd
 from telethon.tl import types
 from userbot import CMD_HELP 
+import re
+import random
+from userbot import bot
 
 
 @borg.on(admin_cmd(pattern="gps ?(.*)"))
@@ -38,7 +41,52 @@ async def gps(event):
         await event.edit("i coudn't find it")
 
 
-CMD_HELP.update({"gps": "`.gps` <location name> :\
-      \nUSAGE: sends you the given location name\
-      "
-})
+IF_EMOJI = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats 
+    "]+")
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(IF_EMOJI, '', inputString)
+
+@borg.on(admin_cmd(pattern="map(?: |$)(.*)"))
+
+async def nope(doit):
+    ok = doit.pattern_match.group(1)
+    if not ok:
+        if doit.is_reply:
+            what = (await doit.get_reply_message()).message
+        
+            return
+    mappy = await bot.inline_query(
+        "openmap_bot", f"{(deEmojify(ok))}")
+    await mappy[0].click(doit.chat_id,
+                            reply_to=doit.reply_to_msg_id,
+                            silent=True if doit.is_reply else False,
+                            hide_via=True)
+    await doit.delete()
+
+
+
+
+
+CMD_HELP.update(
+    {
+        "gps": "__**PLUGIN NAME :** gps__\
+    \n\n📌** CMD ★** `.gps <location>`\
+    \n**USAGE   ★  **Finds your desired location using geopy...\
+    \n\n📌** CMD ★** `.map <location>`\
+    \n**USAGE   ★  **Uses an inline bot to find your desired location.."
+    }
+)
