7e1d9f053a68d8cc4054726246aea2b4b0dd7d6b
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-27 22:54:51 +0530

    Update spotify.py

--- userbot/plugins/spotify.py
+++ userbot/plugins/spotify.py
@@ -1,47 +1,32 @@
 # Plugin made by @okay_retard && @hellboi_atul...
-# If Gonna kang then keep credits..don't edit/remove these lines... Else gay
-# Utilities fixes by @xditya..
-from telethon.errors.rpcerrorlist import UserAlreadyParticipantError
-from telethon.tl.functions.messages import ImportChatInviteRequest
+# If Gonna kang then keep credits..don't edit/remove these lines...
+# else gay
 from telethon.tl.types import InputMessagesFilterMusic
-from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "DARK COBRA"
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.errors.rpcerrorlist import UserAlreadyParticipantError
+
 
 @borg.on(admin_cmd("sptfy ?(.*)"))
 async def _(event):
     try:
-        await event.client(ImportChatInviteRequest("DdR2SUvJPBouSW4QlbJU4g"))
+       await event.client(ImportChatInviteRequest('DdR2SUvJPBouSW4QlbJU4g'))
     except UserAlreadyParticipantError:
         pass
-    except Exception as e:
-        await event.edit(
-            f"`Some unknown error occured!\nAborting...\nError - {str(e)}`"
-        )
+    except:
+        await event.reply("You need to join [this](https://t.me/joinchat/DdR2SUvJPBouSW4QlbJU4g) group for this module to work.", link_preview=False)
         return
-    name = event.pattern_match.group(1)
-    if not name:
-        await event.edit(
-            "Song donwloader.\nSyntax - `.song name`\nFor better results, use Song Name- Artist name."
-        )
+    args = event.pattern_match.group(1)
+    if not args:
+        await event.edit("`Enter song name`")
         return
     chat = -1001271479322
     current_chat = event.chat_id
-    ok = bot.uid
     current_msg = event.id
-    cap = """
-==> **Song name** - `{}`
-==> **Uploaded by** [{DEFAULTUSER}](tg://user?id={ok})
-"""
     try:
-        async for event in event.client.iter_messages(
-            chat, search=name, limit=1, filter=InputMessagesFilterMusic
-        ):
-            ok = cap.format(event.message, DEFAULTUSER)
-            await event.client.delete_messages(current_chat, current_msg)
-            await event.client.send_file(current_chat, event, caption=ok)
-    except BaseException:
-        await event.reply(
-            f"`Song, {name}, not found. For better results, use Song name- Artist Name.`"
-        )
-        return
+       async for event in event.client.iter_messages(chat, search=args, limit=1, filter=InputMessagesFilterMusic):
+                    await event.client.send_file(current_chat, event, caption=event.message)
+    except:
+             await event.reply("`Song not found.`")
+             return
+    await event.client.delete_messages(current_chat, current_msg)
