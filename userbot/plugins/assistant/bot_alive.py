15465ed34361fc691867f72d7fd0d1b2b19e6ddd
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-29 01:35:22 +0530

    Create bot_alive.py

---
+++ userbot/plugins/assistant/bot_alive.py
@@ -0,0 +1,23 @@
+from telethon import events
+
+from userbot import ALIVE_NAME, bot
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+PM_IMG = "https://telegra.ph/file/e6223f1abf7720c4938f3.jpg"
+pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
+pm_caption += "➥ **SYSTEMS STATS**\n"
+pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
+pm_caption += "➥ **Python:** `3.8.6` \n"
+pm_caption += "➥ **Database Status:**  `Functional`\n"
+pm_caption += "➥ **Current Branch** : `master`\n"
+pm_caption += f"➥ **Version** : `2.0`\n"
+pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
+pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/DARK-COBRA/DARKCOBRA/blob/master/LICENSE)\n"
+pm_caption += "➥ **Copyright** : By [DarkCobra](https://t.me/Dark_cobra_support_group)\n"
+pm_caption += "[Assistant By DarkCobra](https://t.me/Dark_cobra_support_group)"
+
+# only Owner Can Use it
+@tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
+async def _(event):
+    await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption)
