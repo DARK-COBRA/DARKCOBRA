66b4aff1f9a9b8dfdaff7ec9d1f78ccafdf84de6
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-29 00:48:09 +0530

    Update main_assistant.py

--- userbot/plugins/assistant/main_assistant.py
+++ userbot/plugins/assistant/main_assistant.py
@@ -82,7 +82,7 @@ async def help(event):
             event.chat_id,
             message="You Can Deploy DARKCOBRA In Heroku By Following Steps Bellow, You Can See Some Quick Guides On Support Channel Or On Your Own Assistant Bot. \nThank You For Contacting Me.",
             buttons=[
-                [Button.url("Deploy Tutorial 📺", "http://www.youtube.com/watch?v=-MbQO6kmP8o],
+                [Button.url("Deploy Tutorial 📺", "http://www.youtube.com/watch?v=-MbQO6kmP8o"],
                 [Button.url("Need Help ❓", "t.me/darkcobra_support")],
             ],
         )
