5f546f74a7ca98e1bfe00ede24efdd87abbe1e81
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-28 22:16:00 +0530

    Update tweet.py

--- userbot/tweet.py
+++ userbot/tweet.py
@@ -76,3 +76,19 @@ async def jtweet(text):
     img = Image.open("hoho.png").convert("RGB")
     img.save("hoho.webp", "webp")
     return "hoho.webp"
+
+async def miatweet(text):
+    r = requests.get(
+        f"https://nekobot.xyz/api/imagegen?type=tweet&text={text}&username=miakhalifa"
+    ).json()
+    ab = r.get("message")
+    cd = url(ab)
+    if not cd:
+        return "check syntax once more"
+    with open("hoho.png", "wb") as f:
+        f.write(requests.get(ab).content)
+    img = Image.open("hoho.png").convert("RGB")
+    img.save("hoho.webp", "webp")
+    return "hoho.webp"
+
+
