9e4257108ed301b3247d46691fb7a164ef82a8dc
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-28 23:42:52 +0530

    Create id_bot.py

---
+++ userbot/plugins/assistant/id_bot.py
@@ -0,0 +1,28 @@
+from telethon import events
+from telethon.utils import pack_bot_file_id
+from userbot import bot
+
+@tgbot.on(events.NewMessage(pattern="^/id"))
+async def _(event):
+    if event.reply_to_msg_id:
+        await event.get_input_chat()
+        r_msg = await event.get_reply_message()
+        if r_msg.media:
+            bot_api_file_id = pack_bot_file_id(r_msg.media)
+            await tgbot.send_message(
+                event.chat_id,
+                "Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.from_id), bot_api_file_id
+                ),
+            )
+        else:
+            await tgbot.send_message(
+                event.chat_id,
+                "Current Chat ID: `{}`\nFrom User ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.from_id)
+                ),
+            )
+    else:
+        await tgbot.send_message(
+            event.chat_id, "Current Chat ID: `{}`".format(str(event.chat_id))
+        )
