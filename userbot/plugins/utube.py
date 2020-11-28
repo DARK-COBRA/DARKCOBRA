b4f36e885cb96cb98ffa3557288ddb7741f61600
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-28 10:57:24 +0530

    Update utube.py

--- userbot/plugins/utube.py
+++ userbot/plugins/utube.py
@@ -30,7 +30,7 @@ def deEmojify(inputString: str) -> str:
     return re.sub(IF_EMOJI, '', inputString)
 
 
-@borg.on(admin_cmd(pattern="utube(?: |$)(.*)"))
+@borg.on(admin_cmd(pattern="utv(?: |$)(.*)"))
 
 async def nope(doit):
     ok = doit.pattern_match.group(1)
