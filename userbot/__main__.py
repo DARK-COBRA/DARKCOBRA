3f6e390d9d86c1e1edd0f278a32b7e28203a36c8
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2020-11-29 08:35:17 +0530

    Added Imports

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -6,7 +6,7 @@ import telethon.utils
 from telethon import TelegramClient
 
 from userbot import bot
-from userbot.utils import load_module
+from userbot.utils import load_module, start_assistant
 from var import Var
 
 LOAD_USERBOT = os.environ.get("LOAD_USERBOT", True)
