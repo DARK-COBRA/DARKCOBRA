83b003e0b9d12435a8e75ae8ec323f65c15740bd
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2020-11-29 08:28:41 +0530

    Update Some Idiotic Stuffs

--- userbot/utils.py
+++ userbot/utils.py
@@ -535,8 +535,8 @@ def start_assistant(shortname):
         import sys
         from pathlib import Path
 
-        path = Path(f"jarvis/plugins/assistant/{shortname}.py")
-        name = "jarvis.plugins.assistant.{}".format(shortname)
+        path = Path(f"userbot/plugins/assistant/{shortname}.py")
+        name = "userbot.plugins.assistant.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
@@ -547,12 +547,12 @@ def start_assistant(shortname):
         import sys
         from pathlib import Path
 
-        path = Path(f"jarvis/plugins/assistant/{shortname}.py")
-        name = "jarvis.plugins.assistant.{}".format(shortname)
+        path = Path(f"userbot/plugins/assistant/{shortname}.py")
+        name = "userbot.plugins.assistant.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.tgbot = bot.tgbot
         spec.loader.exec_module(mod)
-        sys.modules["jarvis.plugins.assistant" + shortname] = mod
+        sys.modules["userbot.plugins.assistant" + shortname] = mod
         print("Assistant Has imported " + shortname)        
 
