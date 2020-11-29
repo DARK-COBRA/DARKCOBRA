f69f5c2662faa9acda27195234f1619cb262eaeb
hellboi-atul <68107352+hellboi-atul@users.noreply.github.com>
2020-11-29 07:37:21 +0530

    Try:

--- userbot/plugins/sangu.py
+++ userbot/plugins/sangu.py
@@ -42,11 +42,7 @@ async def _(event):
               await event.reply("```Please unblock (@Sangmatainfo_bot) ```")
               #team mates @danish_00,@Shivam_Patel,@xditya,@The_Siddharth_Nigam
               return
-          if response3.text.startswith("No records found"):
-             #team mates @danish_00,@Shivam_Patel,@xditya,@The_Siddharth_Nigam
-             await event.edit("```User never changed his name...```")
-             #team mates @danish_00,@Shivam_Patel,@xditya,@AP_XD,@The_Siddharth_Nigam#team mates 
-          else: 
+           
              await event.delete()
              #team mates @danish_00,@Shivam_Patel,@xditya,@AP_XD,@The_Siddharth_Nigam#team mates 
              #team mates @danish_00,@Shivam_Patel,@xditya,@The_Siddharth_Nigam
@@ -84,11 +80,7 @@ async def _(event):
               await event.reply("```Please unblock (@Sangmatainfo_bot) ```")
               #team mates @danish_00,@Shivam_Patel,@xditya,@The_Siddharth_Nigam
               return
-          if response3.text.startswith("No records found"):
-             #team mates @danish_00,@Shivam_Patel,@xditya,@The_Siddharth_Nigam
-             await event.edit("```User never changed his username...```")
-             #team mates @danish_00,@Shivam_Patel,@xditya,@AP_XD,@The_Siddharth_Nigam#team mates 
-          else: 
+           
              await event.delete()
              #team mates @danish_00,@Shivam_Patel,@xditya,@AP_XD,@The_Siddharth_Nigam#team mates 
              #team mates @danish_00,@Shivam_Patel,@xditya,@The_Siddharth_Nigam
