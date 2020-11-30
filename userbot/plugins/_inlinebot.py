620043dd3e4e548ee4788e04d24d37a0a0772800
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-11-30 15:30:12 +0530

    _inlinebot.py

---
+++ userbot/plugins/_inlinebot.py
@@ -0,0 +1,27 @@
+""" 
+
+
+
+Waaah Bc Waaah 
+
+Yahi karo bes tum 😒😒😒😒😒
+
+
+😑😑😑😑😑😑😑🙃🙃🙃🙃🙃🙃😒😒😒😒😒😒😒🤦🏻‍♂️🤦🏻‍♂️
+
+
+
+delete kr diya h _inline
+
+      usko alag se install krna padta h beta
+
+       😎😎😎😎😎
+
+
+"""
+
+
+
+#gtfo
+
+#getlost
