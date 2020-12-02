a9b3e6ae0b160f0df06a0850b1b38b6be2b0c00d
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-12-02 11:09:23 +0530

    🙃🙂

---
+++ userbot/plugins/sys.py
@@ -0,0 +1,225 @@
+##
+
+
+
+
+
+
+
+#
+import platform
+import shutil
+import sys
+import time
+from asyncio import create_subprocess_exec as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+from datetime import datetime
+from os import remove
+from platform import python_version, uname
+from shutil import which
+
+import psutil
+from telethon import __version__, version
+import var as Var
+from userbot import CMD_LIST, ALIVE_NAME, CMD_HELP,  bot
+
+from userbot.events import register
+
+
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+
+@register(outgoing=True, pattern=r"^\.spc")
+async def dc(event):
+    uname = platform.uname()
+    softw = "**System Information**\n"
+    softw += f"`System   : {uname.system}`\n"
+    softw += f"`Release  : {uname.release}`\n"
+    softw += f"`Version  : {uname.version}`\n"
+    softw += f"`Machine  : {uname.machine}`\n"
+    # Boot Time
+    boot_time_timestamp = psutil.boot_time()
+    bt = datetime.fromtimestamp(boot_time_timestamp)
+    softw += f"`Boot Time: {bt.day}/{bt.month}/{bt.year}  {bt.hour}:{bt.minute}:{bt.second}`\n"
+    # CPU Cores
+    cpuu = "**CPU Info**\n"
+    cpuu += "`Physical cores   : " + str(psutil.cpu_count(logical=False)) + "`\n"
+    cpuu += "`Total cores      : " + str(psutil.cpu_count(logical=True)) + "`\n"
+    # CPU frequencies
+    cpufreq = psutil.cpu_freq()
+    cpuu += f"`Max Frequency    : {cpufreq.max:.2f}Mhz`\n"
+    cpuu += f"`Min Frequency    : {cpufreq.min:.2f}Mhz`\n"
+    cpuu += f"`Current Frequency: {cpufreq.current:.2f}Mhz`\n\n"
+    # CPU usage
+    cpuu += "**CPU Usage Per Core**\n"
+    for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
+        cpuu += f"`Core {i}  : {percentage}%`\n"
+    cpuu += "\n**Total CPU Usage**\n"
+    cpuu += f"`All Core: {psutil.cpu_percent()}%`\n"
+    # RAM Usage
+    svmem = psutil.virtual_memory()
+    memm = "**Memory Usage**\n"
+    memm += f"`Total     : {get_size(svmem.total)}`\n"
+    memm += f"`Available : {get_size(svmem.available)}`\n"
+    memm += f"`Used      : {get_size(svmem.used)} ({svmem.percent}%)`\n"
+    # Disk Usage
+    dtotal, dused, dfree = shutil.disk_usage(".")
+    disk = "**Disk Usage**\n"
+    disk += f"`Total     : {get_size(dtotal)}`\n"
+    disk += f"`Free      : {get_size(dused)}`\n"
+    disk += f"`Used      : {get_size(dfree)}`\n"
+    # Bandwidth Usage
+    bw = "**Bandwith Usage**\n"
+    bw += f"`Upload  : {get_size(psutil.net_io_counters().bytes_sent)}`\n"
+    bw += f"`Download: {get_size(psutil.net_io_counters().bytes_recv)}`\n"
+    help_string = f"{str(softw)}\n"
+    help_string += f"{str(cpuu)}\n"
+    help_string += f"{str(memm)}\n"
+    help_string += f"{str(disk)}\n"
+    help_string += f"{str(bw)}\n"
+    help_string += "**Engine Info**\n"
+    help_string += f"`Python {sys.version}`\n"
+    help_string += f"`Telethon {__version__}`"
+    await event.edit(help_string)
+
+
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+
+@register(outgoing=True, pattern=r"^\.sysd$")
+async def sysdetails(sysd):
+    """ For .sysd command, get system info using neofetch. """
+    if not sysd.text[0].isalpha() and sysd.text[0] not in ("/", "#", "@", "!"):
+        try:
+            fetch = await asyncrunapp(
+                "neofetch",
+                "--stdout",
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
+
+            stdout, stderr = await fetch.communicate()
+            result = str(stdout.decode().strip()) + str(stderr.decode().strip())
+
+            await sysd.edit("`" + result + "`")
+        except FileNotFoundError:
+            await sysd.edit("`Install neofetch first !!`")
+
+
+@register(outgoing=True, pattern="^.botver$")
+async def bot_ver(event):
+    """ For .botver command, get the bot version. """
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        if which("git") is not None:
+            ver = await asyncrunapp(
+                "git",
+                "describe",
+                "--all",
+                "--long",
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
+            stdout, stderr = await ver.communicate()
+            verout = str(stdout.decode().strip()) + str(stderr.decode().strip())
+
+            rev = await asyncrunapp(
+                "git",
+                "rev-list",
+                "--all",
+                "--count",
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
+            stdout, stderr = await rev.communicate()
+            revout = str(stdout.decode().strip()) + str(stderr.decode().strip())
+
+            await event.edit(
+                "`Userbot Version: " f"{verout}" "` \n" "`Revision: " f"{revout}" "`"
+            )
+        else:
+            await event.edit(
+                "Don't bother me😑😑 ,you're running - 'DC 2.0' anyway!"
+            )
+
+
+@register(outgoing=True, pattern="^.pip(?: |$)(.*)")
+async def pipcheck(pip):
+    """ For .pip command, do a pip search. """
+    if not pip.text[0].isalpha() and pip.text[0] not in ("/", "#", "@", "!"):
+        pipmodule = pip.pattern_match.group(1)
+        if pipmodule:
+            await pip.edit("`Searching . . .`")
+            pipc = await asyncrunapp(
+                "pip3",
+                "search",
+                pipmodule,
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
+
+            stdout, stderr = await pipc.communicate()
+            pipout = str(stdout.decode().strip()) + str(stderr.decode().strip())
+
+            if pipout:
+                if len(pipout) > 4096:
+                    await pip.edit("`Output too large, sending as file`")
+                    file = open("output.txt", "w+")
+                    file.write(pipout)
+                    file.close()
+                    await pip.client.send_file(
+                        pip.chat_id,
+                        "output.txt",
+                        reply_to=pip.id,
+                    )
+                    remove("output.txt")
+                    return
+                await pip.edit(
+                    "**Query: **\n`"
+                    f"pip3 search {pipmodule}"
+                    "`\n**Result: **\n`"
+                    f"{pipout}"
+                    "`"
+                )
+            else:
+                await pip.edit(
+                    "**Query: **\n`"
+                    f"pip3 search {pipmodule}"
+                    "`\n**Result: **\n`No Result Returned/False`"
+                )
+        else:
+            await pip.edit("`Use .help pip to see an example`")
+
+
+
+
