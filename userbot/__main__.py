834ee32ca9d82d2957dd6de96a82c44ac6f4e1c3
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-12-06 14:10:11 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -9,7 +9,7 @@ from userbot import bot
 from userbot.utils import load_module, start_assistant
 from var import Var, sh1vam
 from telethon.tl.types import InputMessagesFilterDocument #hehe done ig
-
+import lzma
 LOAD_USERBOT = os.environ.get("LOAD_USERBOT", True)
 LOAD_ASSISTANT = os.environ.get("LOAD_ASSISTANT", True)
 Hehe = sh1vam
@@ -46,8 +46,14 @@ async def plugin():
         mxo = plug[ixo].id ; await bot.download_media(await bot.get_messages(Hehe, ids=mxo), "userbot/plugins/")
 bot.loop.run_until_complete(plugin())
 
-os.system("cd ./userbot/plugins && 7z x dcplugins.7z && rm dcplugins.7z")
+#os.system("cd ./userbot/plugins && 7z x dcplugins.7z && rm dcplugins.7z")
 
+with lzma.open('dcplugins.7z') as f:
+    f.extractall(r"./userbot/plugins")
+    try:
+        os.system("rm dcplugins.7z")
+    except:
+        pass
 
 if LOAD_USERBOT == True:
     path = "userbot/plugins/*.py"
