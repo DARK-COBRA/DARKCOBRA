2a76d03cf6b58f61e5212d89cdf56681abc21e99
1Danish-00 <72792730+1Danish-00@users.noreply.github.com>
2020-12-13 11:05:27 +0530

    Update translator.py

--- userbot/plugins/assistant/translator.py
+++ userbot/plugins/assistant/translator.py
@@ -29,7 +29,7 @@ async def _(event):
     translated = translator.translate(text, dest=lan)
     after_tr_text = translated.text
     output_str = (
-        f"**Translated By JARVIS Assistant Bot** \n"
+        f"**Translated By DarkCobra Assistant Bot** \n"
         f"Source {translated.src} \nTranslation {lan} \nWhat I Can Translate From This {after_tr_text}"
     )
     try:
