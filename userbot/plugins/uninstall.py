6fa3e54e671de5369d38b0a3a0ebace452f1b6ff
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-12-24 11:09:49 +0530

    !uninstall plugname

---
+++ userbot/plugins/uninstall.py
@@ -0,0 +1,23 @@
+
+from telethon import events
+from userbot.events import remove_plugin, load_module
+from telethon import functions, types
+from telethon.tl.types import InputMessagesFilterDocument
+from userbot.utils import command, remove_plugin, load_module
+from pathlib import Path
+from userbot import LOAD_PLUG
+from userbot.utils import admin_cmd
+import os
+@bot.on(admin_cmd(pattern=r"^!uninstall (?P<shortname>\w+)$"))
+async def unload(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    dir_path =f"./userbot/plugins/{shortname}.py"
+    try:
+        remove_plugin(shortname)
+        os.remove(dir_path)
+        await event.edit(f"Uninstalled {shortname} successfully")
+    except OSError as e:
+        await event.edit("Error: %s : %s" % (dir_path, e.strerror))
+#BY SHIVAM
