3760f4b9979dd97b1383237d641dd7b901799199
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-12-26 16:57:28 +0530

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -1,6 +1,4 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2
+
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -17,9 +15,13 @@ async def _(event):
     chat = await event.get_input_chat()
     async for x in javes.iter_participants(chat, 100):
         mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    await event.edit(mentions)
-
-
+    #await event.edit(mentions)
+    #await event.delete()
+    if event.reply_to_msg_id:
+        await javes.send_message(event.chat_id,mentions,reply_to=event.reply_to_msg_id)
+    else:
+        await javes.send_message(event.chat_id,mentions)
+    await event.delete()
 @javes.on(admin_cmd(pattern=r"admin", outgoing=True))
 async def _(event):
     if event.fwd_from:
@@ -30,10 +32,10 @@ async def _(event):
     chat = await event.get_input_chat()
     async for x in javes.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    reply_message = None
+    #await event.edit(mentions)
+    #await event.delete()
     if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.edit(mentions)
+        await javes.send_message(event.chat_id,mentions,reply_to=event.reply_to_msg_id)
     else:
-        await event.edit(mentions)
-    
+        await javes.send_message(event.chat_id,mentions)
+    await event.delete()
