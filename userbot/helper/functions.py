64c62b39150e8b346ec7b260eb6da459726fa192
The Terminal <75001577+The-Terminal@users.noreply.github.com>
2021-01-12 08:34:27 +0530

    Update functions.py

--- userbot/helper/functions.py
+++ userbot/helper/functions.py
@@ -1,4 +1,4 @@
-import requests , os, re
+import requests , os, re, time
 from bs4 import BeautifulSoup
 from asyncio import sleep
 from random import choice
@@ -7,6 +7,28 @@ from emoji import get_emoji_regexp
 from PIL import Image
 from validators.url import url
 from telethon.tl.types import Channel
+
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    hmm = len(time_list)
+    for x in range(hmm):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+    time_list.reverse()
+    up_time += ":".join(time_list)
+    return up_time
 
 
 async def darkmusic(dark , QUALITY):
