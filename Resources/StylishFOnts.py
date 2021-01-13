2aa0e5797d6b36cb5448cda33f07678a652f0939
The Terminal <75001577+The-Terminal@users.noreply.github.com>
2021-01-13 16:48:49 +0530

    Create StylishFOnts.py

---
+++ Resources/StylishFOnts.py
@@ -0,0 +1,30 @@
+#Kang karne aaye ho😂😂 Karlo Kang ji bharke
+
+
+
+
+
+
+
+
+
+#Saala Meowdharchod
+
+
+
+
+#CHutiyapa karte ho
+
+
+
+
+#Nikal gemdu aumrat kahiki
+
+
+
+
+
+
+
+
+#Warna GBAN milega Bhosadike
