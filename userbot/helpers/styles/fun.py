4bef0842d1a3c802c530174eaf2528026f7dc6d0
The Terminal <75001577+The-Terminal@users.noreply.github.com>
2021-01-13 16:42:27 +0530

    Create fun.py

---
+++ userbot/helpers/styles/fun.py
@@ -0,0 +1,20 @@
+from userbot.functions.funts import DC_STYLE as DC_FONT_STYLE
+from userbot.functions.funts import DARKFONTS as DCFONTS
+from userbot.functions.funts import SIMPLESTDC as SIMPLEDC
+
+
+
+
+
+
+
+
+
+#Kanger
+
+
+#tEri maa ki chut
+
+
+
+#useless
