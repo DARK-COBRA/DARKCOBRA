83f3bce75c370124a011c4c4755bb27ef825e027
hellboi_atul <68107352+hellboi-atul@users.noreply.github.com>
2021-01-22 20:58:14 +0530

    Update power_tools.py

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -10,10 +10,11 @@ from telethon import events
 import asyncio
 import os
 import sys
-from uniborg.util import admin_cmd
+from uniborg.util import admin_cmd, sudo_cmd
 
 
 @borg.on(admin_cmd(pattern="restart"))
+@borg.on(sudo_cmd(pattern="restart"))
 async def _(event):
     await event.edit("Restarting ▰▱▱▱▱▱▱▱18%...")
     await asyncio.sleep(1)
@@ -29,6 +30,7 @@ async def _(event):
 
 
 @borg.on(admin_cmd(pattern="shutdown"))
+@borg.on(sudo_cmd(pattern="shutdown"))
 async def _(event):
     if event.fwd_from:
         return
