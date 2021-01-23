3ca467bb11b94115fec4f17e9c9bdd7b92932492
hellboi_atul <68107352+hellboi-atul@users.noreply.github.com>
2021-01-23 22:56:49 +0530

    Bot

--- userbot/plugins/eval.py
+++ userbot/plugins/eval.py
@@ -8,7 +8,7 @@ import traceback
 import asyncio
 import sys
 import io
-from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply as eor
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply 
 from userbot import CMD_HELP
 
 @bot.on(admin_cmd("eval"))
