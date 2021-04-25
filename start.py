cc07bdaf01df8fd5b8a87b954e4542daf0a6ee3a
Danish <danish@ultroid.tech>
2021-04-25 09:31:16 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,15 @@
+import subprocess
+process = subprocess.Popen(
+        ["git", "clone", "https://github.com/DARK-COBRA/DARKCOBRA", "/root/cobra"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,)
+process.communicate()
+
+process = subprocess.Popen(
+        ["cd", "root/cobra", "&&", "python3", "-m", "userbot"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,)
+out, er = process.communicate()
+print(er)
+print("::::::::::::::")
+print(out)
