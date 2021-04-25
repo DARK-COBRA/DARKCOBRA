f229a5c44d68dac26d2fc0984d9a1cdc8fb9d038
Danish <danish@ultroid.tech>
2021-04-25 23:25:08 +0530

    Update sizeresize.py

--- userbot/plugins/sizeresize.py
+++ userbot/plugins/sizeresize.py
@@ -26,21 +26,16 @@ async def __(event):
 @bot.on(admin_cmd(pattern="resize ?(.*)", outgoing=True))
 # DONOT KANG by Sh1vam
 async def __(event):
-    
     path = "shivamdownloades"
     licence = event.text
     liscence=licence[8:]
     await event.delete()
     reply = await event.get_reply_message()
-    
     download = await borg.download_media(reply.media, path)
-    #image = PIL.Image.open(download)
     shvm=PIL.Image.open(download)
     shi,vam = liscence.split(":")
     img=shvm.resize((int(shi),int(vam)))
-    #await event.edit(f"Dimensions Of Image are {shi} by {vam}")
     img.save("sh1vam.png", format="PNG", optimize=True)
-    await event.client.send_file(event.chat_id, "sh1vam.png", force_document=True, reply_to=event.reply_to_msg_id)
     await event.client.send_file(event.chat_id, "sh1vam.png", force_document=False, reply_to=event.reply_to_msg_id)
     os.remove(download)
     os.remove("sh1vam.png")
