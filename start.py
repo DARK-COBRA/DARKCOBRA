7e59aab1f9c6a6886a1287680ace5e00272aa2a2
Danish <danish@ultroid.tech>
2021-04-25 17:17:33 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,19 +1,14 @@
-import subprocess
-process = subprocess.Popen(
-        ["git", "clone", "https://github.com/DARK-COBRA/DARKCOBRA", "root/cobra"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,)
-process.communicate()
-
 import os 
-
-os.chdir(os.path.abspath(os.path.expanduser('root/cobra')))
+import subprocess
+os.system("git clone https://github.com/DARK-COBRA/DARKCOBRA /root/cobra && cd /root/cobra")
 
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,)
 out, er = process.communicate()
-print(er)
+if er:
+    print(er.decode())
 print("::::::::::::::")
-print(out)
+if out:
+    print(out.decode())
